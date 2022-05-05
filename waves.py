f78d672b513b53baa0bdb30425887bc77a25a876
Kyle Brindley <kbrindley@lanl.gov>
2022-05-05 08:07:51 -0600

    PEP-8 line spacing and internal function name

--- waves.py
+++ waves.py
@@ -4,7 +4,8 @@ import pathlib
 
 import SCons.Builder
 
-def abaqus_journal_modify_targets(target, source, env):
+
+def _abaqus_journal_emitter(target, source, env):
     """Appends the abaqus_journal builder target list with the builder managed targets
 
     Appends ``source[0]``.jnl and ``source[0]``.log to the ``target`` list. The abaqus_journal Builder requires that the
@@ -24,4 +25,4 @@ def abaqus_journal_modify_targets(target, source, env):
 abaqus_journal = SCons.Builder.Builder(
     chdir=1,
     action='abaqus cae -noGui ${SOURCE.abspath} -- ${journal_options} > ${SOURCE.filebase}.log 2>&1',
-    emitter=abaqus_journal_modify_targets)
+    emitter=_abaqus_journal_emitter)
