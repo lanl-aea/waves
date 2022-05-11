942330521069e0767ffae46124c3c969f85c36e8
Kyle Brindley <kbrindley@lanl.gov>
2022-05-11 11:11:33 -0600

    WIP: parametrize the expected target list

--- waves/tests/test_builders.py
+++ waves/tests/test_builders.py
@@ -6,16 +6,16 @@ import pytest
 from waves import builders
 
 copy_substitute_input = {
-    'strings': (['dummy']),
-    'pathlib.Path()s': ([pathlib.Path('dummy')]),
+    'strings': (['dummy'], ['dummy']),
+    'pathlib.Path()s': ([pathlib.Path('dummy')], ['dummy']),
 }
 
 
 @pytest.mark.unittest
-@pytest.mark.parametrize('source_list',
+@pytest.mark.parametrize('source_list, expected_list',
                          copy_substitute_input.values(),
                          ids=copy_substitute_input.keys())
-def test__copy_substitute(source_list):
+def test__copy_substitute(source_list, expected_list):
     target_list = builders.copy_substitute(source_list, {})
     target_files = [str(target) for target in target_list]
-    assert target_files == ['dummy'] 
+    assert target_files == expected_list
