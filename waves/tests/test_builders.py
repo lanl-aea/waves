2291f9bf20be3cc1d6cfa93a88a4482701280c1c
Kyle Brindley <kbrindley@lanl.gov>
2022-05-11 11:04:30 -0600

    WIP: parameterized unit test

--- waves/tests/test_builders.py
+++ waves/tests/test_builders.py
@@ -5,6 +5,14 @@ import pytest
 
 from waves import builders
 
+copy_substitute_input = {
+    'strings': (['dummy'])
+}
 
-def test__copy_substitute():
-    builders.copy_substitute(['dummy'], {})
+
+@pytest.mark.unittest
+@pytest.mark.parametrize('source_list',
+                         copy_substitute_input.values(),
+                         ids=copy_substitute_input.keys())
+def test__copy_substitute(source_list):
+    builders.copy_substitute(source_list, {})
