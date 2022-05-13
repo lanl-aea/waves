d6925bd2f9df4dcbd419a04428c0886a4a8f3c4c
Kyle Brindley <kbrindley@lanl.gov>
2022-05-13 13:53:45 -0600

    WIP: resolve package paths in settings. Condense package internal meta paths

--- waves/_settings.py
+++ waves/_settings.py
@@ -1,4 +1,8 @@
+import pathlib
+
 # Set some semi-private project meta variables for package internal use to avoid hardcode duplication
-_project_name = "WAVES Analysis for Validated Engineering Simulations"
-_project_name_short = "WAVES"
-_project_bin_dir = "bin"
+_project_root_abspath = pathlib.Path(__file__).parent.resolve()
+_project_name_short = _project_root_abspath.name.upper() 
+_project_name = f"{_project_name_short} Analysis for Validated Engineering Simulations"
+_project_bin_dir = _project_root_abspath / 'bin' 
+_abaqus_wrapper = _project_bin_dir / 'abaqus_wrapper'
