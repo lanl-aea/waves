8e9badbe3ca4cad085241e8e1f75dff720a7cced
Kyle Brindley <kbrindley@lanl.gov>
2022-05-13 12:55:46 -0600

    WIP: add project meta data to a settings file

---
+++ waves/_settings.py
@@ -0,0 +1,4 @@
+# Set some semi-private project meta variables for package internal use to avoid hardcode duplication
+_project_name = "WAVES Analysis for Validated Engineering Simulations"
+_project_name_short = "WAVES"
+_project_bin_dir = "bin"
