eb96fe5bf8d0a1fb25ff124afda18d9b7ed2ca84
Kyle Brindley <kbrindley@lanl.gov>
2022-05-16 17:56:56 -0600

    WIP: update tests to match new generate output. Still need to investigate the generated types. It looks like everything comes in as strings

--- waves/tests/test_cartesian_product.py
+++ waves/tests/test_cartesian_product.py
@@ -2,6 +2,7 @@
 """
 
 import pathlib
+import numpy
 import pytest
 
 from waves.parameter_generators import CartesianProduct
@@ -10,20 +11,22 @@ class TestCartesianProduct:
     """Class for testing CartesianProduct parameter study generator class"""
 
     generate_io = {
-        'one_parameter': ({'parameter_1': [1, 2]}, ['parameter_1: 1\n', 'parameter_1: 2\n']),
+        'one_parameter': ({'parameter_1': [1, 2]},
+                          numpy.array([['1', '2']])),
         'two_parameter': ({'parameter_1': [1, 2], 'parameter_2': ['a', 'b']},
-                          ['parameter_1: 1\nparameter_2: a\n',
-                           'parameter_1: 1\nparameter_2: b\n',
-                           'parameter_1: 2\nparameter_2: a\n',
-                           'parameter_1: 2\nparameter_2: b\n'])
+                          numpy.array(
+                              [["1", "a"],
+                               ["1", "b"],
+                               ["2", "a"],
+                               ["2", "b"]]))
     }
 
     @pytest.mark.unittest
-    @pytest.mark.parametrize('parameter_schema, expected_text_list',
+    @pytest.mark.parametrize('parameter_schema, expected_array',
                                  generate_io.values(),
                              ids=generate_io.keys())
-    def test_generate(self, parameter_schema, expected_text_list):
+    def test_generate(self, parameter_schema, expected_array):
         TestGenerate = CartesianProduct(parameter_schema, None, False, False, False)
-        parameter_set_file_paths = TestGenerate.generate()
-        assert list(TestGenerate.parameter_study.values()) == expected_text_list
-        assert parameter_set_file_paths == {pathlib.Path(f"parameter_set{num}"): expected_text_list[num] for num in range(len(expected_text_list))}
+        TestGenerate.generate()
+        assert TestGenerate.parameter_study.sel(parameter_data='values').to_array().values == expected_array
+        assert parameter_set_file_paths == [pathlib.Path(f"parameter_set{num}") for num in range(len(expected_text_list))]
