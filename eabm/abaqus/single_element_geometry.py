3884dfddcad97b29a4a6baa13470771d6eba1eba
Kyle Brindley <kbrindley@lanl.gov>
2022-05-26 14:30:56 -0600

    DOC: match the API arguments messages in the CLI. hardcoded for now until/unless I can find a sustainable solution for inserting variables in a docstring or retrieving argument messages from the docstring. Matt Fister recommended a regex solution to pull argument descriptions out of the docstring for re-use.

--- eabm/abaqus/single_element_geometry.py
+++ eabm/abaqus/single_element_geometry.py
@@ -7,15 +7,15 @@ import abaqus
 import abaqusConstants
 
 def main(output_file, model_name, part_name, width, height):
-    """Create a simple square geometry.
+    """Create a simple rectangle geometry.
 
-    This script creates a simple Abaqus model with a single square part.
+    This script creates a simple Abaqus model with a single rectangle part.
 
     :param str output_file: The output file for the Abaqus model without extension, e.g. ``output_file``.cae
     :param str model_name: The name of the Abaqus model
     :param str part_name: The name of the Abaqus part
-    :param float width: The square width
-    :param float height: The square height
+    :param float width: The rectangle width
+    :param float height: The rectangle height
 
     :returns: writes ``output_file``.cae
     """
@@ -56,19 +56,19 @@ def get_parser():
     default_height = 1.0
     
     prog = "abaqus cae -noGui {} --".format(basename)
-    parser = argparse.ArgumentParser(description="Create a simple square geometry",
+    parser = argparse.ArgumentParser(description="Create a simple rectangle geometry",
                                      prog=prog)
     parser.add_argument('-o', '--output-file', type=str, default=default_output_file,
-                        help="output file name")
+                        help="The output file for the Abaqus model without extension, e.g. ``output_file``.cae")
     parser.add_argument('-m', '--model-name', type=str, default=default_part_name,
-                        help="model name")
+                        help="The name of the Abaqus model")
     parser.add_argument('-p', '--part-name', type=str, default=default_part_name,
-                        help="part name")
+                        help="The name of the Abaqus part")
     parser.add_argument('-w', '--width', type=float, default=default_width,
-                        help="square width")
+                        help="The rectangle width")
     # Short option '-h' is reserved for the help message
     parser.add_argument('--height', type=float, default=default_height,
-                        help="square height")
+                        help="The rectangle height")
     return parser
 
 
