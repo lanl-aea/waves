11613f3b412435808f264fe04d2ea07a882be3d5
Kyle Brindley <kbrindley@lanl.gov>
2022-05-26 14:47:37 -0600

    DOC: make the mesh journal file api and cli consistent

--- eabm/abaqus/single_element_mesh.py
+++ eabm/abaqus/single_element_mesh.py
@@ -31,18 +31,9 @@ def export_mesh(model_object, part_name, orphan_mesh_file):
         output.write(part_definition[1].strip())
 
 def main(input_file, output_file, model_name, part_name, global_seed):
-    """Mesh the simple square geometry partitioned by single_element_partition.py
+    """Mesh the simple square geometry partitioned by ``single_element_partition.py``
 
     This script meshes a simple Abaqus model with a single square part.
-
-    **Usage:**
-
-    .. code-block::
-
-       abaqus cae -noGUI single_element_mesh.py
-       abaqus cae -noGUI single_element_mesh.py -- [options]
-       abaqus cae -noGUI single_element_mesh.py -- --output-file single_element_partition.cae
-       abaqus cae -noGUI single_element_mesh.py -- --part-name single_element.cae
 
     **Node sets:**
 
@@ -111,18 +102,21 @@ if __name__ == '__main__':
     default_output_file = '{}'.format(basename_without_extension)
     default_global_seed = 1.0
 
-    parser = argparse.ArgumentParser(description="Mesh a simple square geometry",
+    cli_description = "Mesh the simple square geometry partitioned by ``single_element_partition.py`` " \
+                      "and write an ``output_file``.cae Abaqus model file."
+    parser = argparse.ArgumentParser(description=cli_description,
                                      prog=os.path.basename(filename))
     parser.add_argument('-i', '--input-file', type=str, default=default_input_file,
-                        help="input file name")
+                        help="The Abaqus model file created by single_element_partition.py without extension, " \
+                             "e.g. ``input_file``.cae")
     parser.add_argument('-o', '--output-file', type=str, default=default_output_file,
-                        help="output file name")
+                        help="The output file for the Abaqus model without extension, e.g. ``output_file``.cae")
     parser.add_argument('-m', '--model-name', type=str, default=default_part_name,
-                        help="model name")
+                        help="The name of the Abaqus model")
     parser.add_argument('-p', '--part-name', type=str, default=default_part_name,
-                        help="part name")
+                        help="The name of the Abaqus part")
     parser.add_argument('-g', '--global-seed', type=float, default=default_global_seed,
-                        help="global mesh seed size")
+                        help="The global mesh seed size")
 
     # Abaqus does not strip the CAE options, so we have to skip the unknown options related to the CAE CLI.
     args, unknown = parser.parse_known_args()
