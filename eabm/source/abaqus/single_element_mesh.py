6d56f9e4801c15b62bf73bf1f3478e373d3c2f1d
Kyle Brindley <kbrindley@lanl.gov>
2022-05-27 14:05:09 -0600

    WIP: Use common eabm utitilies

--- eabm/source/abaqus/single_element_mesh.py
+++ eabm/source/abaqus/single_element_mesh.py
@@ -9,26 +9,10 @@ import abaqus
 import abaqusConstants
 import mesh
 
-def export_mesh(model_object, part_name, orphan_mesh_file):
-    """Export an orphan mesh for the specified part instance in an Abaqus model
+filename = inspect.getfile(lambda: None)
+sys.path.insert(0, os.path.dirname(filename))
+from utilities import export_mesh
 
-    Using an abaqus model object (``model_object = abaqus.mdb.models[model_name]``) with part(s) that are meshed and
-    instanced in an assembly, get the "\*.inp" keyword blocks and save an orphan mesh file, ``orphan_mesh_file``.inp, for
-    the specific ``part_name``.
-
-    :param abaqus.mdb.models[model_name] model_object: Abaqus model object
-    :param str part_name: Part name to export as an orphan mesh
-    :param str orphan_mesh_file: File name to write for the orphan mesh without extension, e.g. ``orphan_mesh_file``.inp
-
-    :returns: writes ``orphan_mesh_file``.inp
-    """
-    model_object.keywordBlock.synchVersions()
-    block = model_object.keywordBlock.sieBlocks
-    block_string = '\n'.join(block)
-    orphan_mesh = re.findall(".*?\*Part, name=({})$\n(.*?)\*End Part".format(part_name), block_string, re.DOTALL | re.I | re.M)
-    part_definition = orphan_mesh[0]
-    with open('{}.inp'.format(orphan_mesh_file), 'w') as output:
-        output.write(part_definition[1].strip())
 
 def main(input_file, output_file, model_name, part_name, global_seed):
     """Mesh the simple rectangle geometry partitioned by ``single_element_partition.py``
@@ -89,7 +73,6 @@ def main(input_file, output_file, model_name, part_name, global_seed):
 
 if __name__ == '__main__':
     # The global '__file__' variable doesn't appear to be set when executing from Abaqus CAE
-    filename = inspect.getfile(lambda: None)
     basename = os.path.basename(filename)
     basename_without_extension, extension = os.path.splitext(basename)
     # Construct a part name from the filename less the workflow step
