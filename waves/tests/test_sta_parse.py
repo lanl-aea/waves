cb630b9f1396cfafd60d942d40706051ef114980
Prabhu Khalsa <pkhalsa@pn1934993.lanl.gov>
2022-05-31 10:12:29 -0600

    WIP: Initial check in of odb_extract code directly from ecmf. Updates forthcoming.

---
+++ waves/tests/test_sta_parse.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test sta parse
+Test sta_parse.py
+
+.. moduleauthor:: Prabhu S. Khalsa <pkhalsa@lanl.gov>
+"""
+
+from unittest.mock import patch
+
+from ecmf.command_line_tools import sta_parse
+
+
+def test_get_parser():
+    with patch('sys.argv', ['sta_parse.py', 'sample.sta', '-o', 'sample.yaml']):
+        cmd_args = sta_parse.get_parser().parse_args()
+        assert cmd_args.sta_file[0] == "sample.sta"
+        assert cmd_args.output_file == "sample.yaml"
+
+
+def test_main(caplog):
+    with patch('sys.argv', ['sta_parse.py', 'sample.sta']), \
+         patch('ecmf.work.abaqus_file_parser.StaFileParser'):
+        sta_parse.main()
+    critical_records = [r.message for r in caplog.records if r.levelname == 'CRITICAL']
+    assert "sample.sta does not exist" in critical_records[0]
+    caplog.clear()
+
+    path_exists = [True, True]
+    with patch('sys.argv', ['sta_parse.py', 'sample.sta']), \
+         patch('pathlib.Path.exists', side_effect=path_exists), \
+         patch('ecmf.work.abaqus_file_parser.StaFileParser.write_yaml') as write_yaml, \
+         patch('ecmf.work.abaqus_file_parser.StaFileParser.parse') as parse:
+        sta_parse.main()
+        assert parse.called
+        assert write_yaml.called
+
+    warning_logs = [r.message for r in caplog.records if r.levelname == 'WARNING']
+    assert "already exists" in warning_logs[0]
+    caplog.clear()
+
+
