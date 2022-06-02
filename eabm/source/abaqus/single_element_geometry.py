423177e3b93bc48ddc6603fa38ecc1e52667f255
Thomas Phillip Roberts <tproberts@lanl.gov>
2022-06-02 14:06:56 -0600

    MAINT: add blank lines around markers

--- eabm/source/abaqus/single_element_geometry.py
+++ eabm/source/abaqus/single_element_geometry.py
@@ -38,6 +38,7 @@ def main(output_file, model_name, part_name, width, height):
     abaqus.mdb.saveAs(pathName='{}.cae'.format(output_file))
 
     return 0
+
 # Comment used in tutorial code snippets: marker-1
 
 
@@ -72,6 +73,7 @@ def get_parser():
     parser.add_argument('--height', type=float, default=default_height,
                         help="The rectangle height")
     return parser
+
 # Comment used in tutorial code snippets: marker-2
 
 if __name__ == '__main__':
