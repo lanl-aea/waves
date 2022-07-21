7b99de88adb1ee41b5078257b7ec1c03cad83b56
Kyle Brindley <kbrindley@lanl.gov>
2022-07-21 09:24:59 -0600

    TST: test for expected parameter schema used by latin hypercube parameter generator

---
+++ waves/tests/test_latinhypercube.py
@@ -0,0 +1,24 @@
+"""Test LatinHypercube Class
+"""
+
+import pytest
+from contextlib import nullcontext as does_not_raise
+
+from waves.parameter_generators import LatinHypercube
+
+class TestLatinHypercube:
+    """Class for testing LatinHypercube parameter study generator class"""
+
+    validate_input = {
+        "good schema": (
+            {'num_simulations': 2, 'parameter_1': {'distribution': 'norm', 'kwarg1': 1}},
+            does_not_raise())
+    }
+
+    @pytest.mark.unittest
+    @pytest.mark.parametrize('parameter_schema, outcome',
+                             validate_input.values(),
+                             ids=validate_input.keys())
+    def test__validate(self, parameter_schema, outcome):
+        # Validate is called in __init__. Do not need to call explicitly.
+        TestValidate = LatinHypercube(parameter_schema, None, False, False, False)
