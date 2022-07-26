9d4f426c1061f6b52e5ca62dd8b327679f438f2d
Kyle Brindley <kbrindley@lanl.gov>
2022-07-26 13:00:36 -0600

    FEAT: distinguish between optional and required named arguments. Add units as required command line options

--- eabm/eabm_package/python/plot_scatter.py
+++ eabm/eabm_package/python/plot_scatter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""stress strain comparison"""
+"""field output scatter plots"""
 
 import sys
 import argparse
@@ -9,13 +9,9 @@ import xarray
 import matplotlib.pyplot
 
 
-def main(input_files, output_file, group_path):
+def main(input_files, output_file, group_path, x_var, x_units, y_var, y_units):
     # TODO: Move dataset meta script assumptions to CLI
     select_dict = {"LE values": "LE22", "S values": "S22", "elements": 1, "step": "Step-1"}
-    x_var = "LE"
-    x_units = "mm/mm"
-    y_var = "S"
-    y_units = "MPa"
     concat_coord = "parameter_sets"
 
     # Build single dataset along the "parameter_sets" dimension
@@ -42,25 +38,43 @@ def get_parser():
     script_name = pathlib.Path(__file__)
     default_output_file = script_name.stem
     default_group_path = "SINGLE_ELEMENT/FieldOutputs/ALL"
+    default_x_var = "LE"
+    default_y_var = "S"
 
     prog = f"python {script_name.name} "
     cli_description = "Read Xarray Datasets and plot stress-strain comparisons as a function of parameter set name. " \
                       " Save to ``output_file``."
     parser = argparse.ArgumentParser(description=cli_description,
                                      prog=prog)
-    parser.add_argument("-i", "--input-files", nargs="+",
-                        help="The Xarray Dataset file(s)")
+    required_named = parser.add_argument_group('required named arguments')
+    required_named.add_argument("-i", "--input-file", nargs="+", required=True,
+                                help="The Xarray Dataset file(s)")
+    required_named.add_argument("--x-units", type=str, required=True,
+                                help="The dependent (x-axis) units string.")
+    required_named.add_argument("--y-units", type=str, required=True,
+                                help="The dependent (y-axis) units string.")
+
     parser.add_argument("-o", "--output-file", type=str, default=default_output_file,
                         help="The output file for the stress-strain comparison plot with extension, " \
-                             "e.g. ``output_file.pdf``. Extension must be supported by matplotlib.")
+                             "e.g. ``output_file.pdf``. Extension must be supported by matplotlib. " \
+                             "(default: %(default)s)")
     parser.add_argument("-g", "--group-path", type=str, default=default_group_path,
                         help="The h5py group path to the dataset object (default: %(default)s)")
+    parser.add_argument("-x", "--x-var", type=str, default=default_x_var,
+                        help="The dependent (x-axis) variable name (default: %(default)s)")
+    parser.add_argument("-y", "--y-var", type=str, default=default_y_var,
+                        help="The independent (y-axis) variable name (default: %(default)s)")
+
     return parser
 
 
 if __name__ == "__main__":
     parser = get_parser()
     args, unknown = parser.parse_known_args()
-    sys.exit(main(input_files=args.input_files,
+    sys.exit(main(input_files=args.input_file,
                   output_file=args.output_file,
-                  group_path=args.group_path))
+                  group_path=args.group_path,
+                  x_var=args.x_var,
+                  x_units=args.x_units,
+                  y_var=args.y_var,
+                  y_units=args.y_units))
