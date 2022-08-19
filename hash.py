257d70d3a65ef1c0fd13e26ca3f2054ef45664f6
Kyle Brindley <kbrindley@lanl.gov>
2022-08-19 08:08:56 -0600

    WIP: generate function to explore set name assignment order w.r.t. study creation

--- hash.py
+++ hash.py
@@ -1,15 +1,22 @@
 import hashlib
+import string
 
 import xarray
 import numpy
+
+
+class _AtSignTemplate(string.Template):
+    """Use the CMake '@' delimiter in a Python 'string.Template' to avoid clashing with bash variable syntax"""
+    delimiter = '@'
+
 
 class ParameterStudy():
 
     def __init__(self, data):
         self.samples = data
+        self.set_name_template = _AtSignTemplate("set@number")
 
-        self.parameter_set_names = [f"set{number}" for number in range(self.samples.shape[0])]
-        self.parameter_names = [f"parameter_{number}" for number in range(self.samples.shape[1])]
+    def _create_parameter_set_hashes(self):
 
         self.parameter_set_hashes = []
         for row in self.samples:
@@ -17,7 +24,18 @@ class ParameterStudy():
             set_hash = hashlib.md5(set_values_catenation.encode('utf-8')).hexdigest()
             self.parameter_set_hashes.append(set_hash)
 
-        self._create_parameter_study()
+    def _create_parameter_set_names(self, set_count):
+        """Construct parameter set names from the output file template and number of parameter sets
+
+        Creates the class attribute ``self.parameter_set_names`` required to populate the ``generate()`` method's
+        parameter study Xarray dataset object.
+
+        :param int set_count: Integer number of parameter sets
+        """
+        self.parameter_set_names = []
+        for number in range(set_count):
+            template = self.set_name_template
+            self.parameter_set_names.append(template.substitute({'number': number}))
 
     def _create_parameter_array(self, data, name):
         """Create the standard structure for a parameter_study array
@@ -64,19 +82,28 @@ class ParameterStudy():
         else:
             self.parameter_study = samples.to_dataset("parameters").expand_dims(data_type=["samples"])
 
+    def generate(self):
+        self._create_parameter_set_names(self.samples.shape[0])
+        self.parameter_names = [f"parameter_{number}" for number in range(self.samples.shape[1])]
+        self._create_parameter_set_hashes()
+        self._create_parameter_study()
+
+
 if __name__ == "__main__":
     print('\nStudy1:')
     data1 = numpy.array([[1, 10.1, 'a'], [2, 20.2, 'b'], [4, 40.4, 'd']], dtype=object)
     study1 = ParameterStudy(data1)
+    study1.generate()
     print(study1.parameter_study)
     print("")
     for set_name, set_hash, row in zip(study1.parameter_set_names, study1.parameter_set_hashes, study1.samples):
         print(f"{set_name}: {set_hash}: {row}")
 
-    
+
     print('\nStudy2:')
     data2 = numpy.array([[1, 10.1, 'a'], [3, 30.3, 'c'], [5, 50.5, 'e'], [2, 20.2, 'b']], dtype=object)
     study2 = ParameterStudy(data2)
+    study2.generate()
     print(study2.parameter_study)
     print("")
     for set_name, set_hash, row in zip(study2.parameter_set_names, study2.parameter_set_hashes, study2.samples):
