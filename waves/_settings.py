304d939882cb75da6a3d5adc7de64cb97df9a1fd
Kyle Brindley <kbrindley@lanl.gov>
2022-08-23 08:20:11 -0600

    MAINT: remove trailing white space

--- waves/_settings.py
+++ waves/_settings.py
@@ -2,9 +2,9 @@ import pathlib
 
 # Set some semi-private project meta variables for package internal use to avoid hardcode duplication
 _project_root_abspath = pathlib.Path(__file__).parent.resolve()
-_project_name_short = _project_root_abspath.name.upper() 
+_project_name_short = _project_root_abspath.name.upper()
 _project_name = f"{_project_name_short} Analysis for Verified Engineering Simulations"
-_project_bin_dir = _project_root_abspath / "bin" 
+_project_bin_dir = _project_root_abspath / "bin"
 _abaqus_environment_file = "abaqus_v6.env"
 _abaqus_environment_extension = f".{_abaqus_environment_file}"
 _abaqus_solver_common_suffixes = [".odb", ".dat", ".msg", ".com", ".prt"]
