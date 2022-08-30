632b71911e38b6f5357b9bbb68c23d5a6b32cd62
Kyle Brindley <kbrindley@lanl.gov>
2022-08-30 15:40:05 -0600

    WIP: test expected merge behavior

--- waves/tests/test_sobol_sequence.py
+++ waves/tests/test_sobol_sequence.py
@@ -8,7 +8,7 @@ import pytest
 import numpy
 import scipy.stats
 
-from waves.parameter_generators import SobolSequence 
+from waves.parameter_generators import SobolSequence
 from waves._settings import _hash_coordinate_key, _set_coordinate_key
 
 
@@ -62,8 +62,6 @@ class TestSobolSequence:
             finally:
                 pass
 
-    generate_input_interface = "parameter_schema, expected_samples, expected_quantiles"
-
     generate_input = {
         "good schema 5x2": (
             {'num_simulations': 5,
@@ -96,7 +94,7 @@ class TestSobolSequence:
     }
 
     @pytest.mark.unittest
-    @pytest.mark.parametrize(generate_input_interface,
+    @pytest.mark.parametrize("parameter_schema, expected_samples, expected_quantiles",
                              generate_input.values(),
                              ids=generate_input.keys())
     def test_generate(self, parameter_schema, expected_samples, expected_quantiles):
@@ -114,3 +112,63 @@ class TestSobolSequence:
         expected_set_names = [f"parameter_set{num}" for num in range(parameter_schema['num_simulations'])]
         parameter_set_names = list(TestGenerate.parameter_study[_set_coordinate_key])
         assert numpy.all(parameter_set_names == expected_set_names)
+
+    merge_test = {
+        'new sets': (
+            {'num_simulations': 5, 'parameter_1': [0., 10.], 'parameter_2': [2.,  5.]},
+            {'num_simulations': 8, 'parameter_1': [0., 10.], 'parameter_2': [2.,  5.]},
+            # Ordered by md5 hash during Xarray merge operation. New tests must verify hash ordering.
+            numpy.array([[0.   , 2.   ],
+                         [5.   , 3.5  ],
+                         [7.5  , 2.75 ],
+                         [2.5  , 4.25 ],
+                         [3.75 , 3.125],
+                         [8.75 , 4.625],
+                         [6.25 , 2.375],
+                         [1.25 , 3.875]], dtype=object),
+            numpy.array([[0.   , 0.   ],
+                         [0.5  , 0.5  ],
+                         [0.75 , 0.25 ],
+                         [0.25 , 0.75 ],
+                         [0.375, 0.375],
+                         [0.875, 0.875],
+                         [0.625, 0.125],
+                         [0.125, 0.625]], dtype=object)
+        ),
+        'unchanged sets': (
+            {'num_simulations': 5, 'parameter_1': [0., 10.], 'parameter_2': [2.,  5.]},
+            {'num_simulations': 5, 'parameter_1': [0., 10.], 'parameter_2': [2.,  5.]},
+            # Ordered by md5 hash during Xarray merge operation. New tests must verify hash ordering.
+            numpy.array([[0.   , 2.   ],
+                         [5.   , 3.5  ],
+                         [7.5  , 2.75 ],
+                         [2.5  , 4.25 ],
+                         [3.75 , 3.125]]),
+            numpy.array([[0.   , 0.   ],
+                         [0.5  , 0.5  ],
+                         [0.75 , 0.25 ],
+                         [0.25 , 0.75 ],
+                         [0.375, 0.375]])
+        )
+    }
+
+    @pytest.mark.unittest
+    @pytest.mark.parametrize('first_schema, second_schema, expected_samples, expected_quantiles',
+                                 merge_test.values(),
+                             ids=merge_test.keys())
+    def test_merge(self, first_schema, second_schema, expected_samples, expected_quantiles):
+        TestMerge1 = SobolSequence(first_schema)
+        TestMerge1.generate()
+        with patch('xarray.open_dataset', return_value=TestMerge1.parameter_study):
+            TestMerge2 = SobolSequence(second_schema, previous_parameter_study='dummy_string')
+            TestMerge2.generate()
+        samples_array = TestMerge2._samples
+        quantiles_array = TestMerge2._quantiles
+        assert numpy.allclose(samples_array, expected_samples)
+        assert numpy.allclose(quantiles_array, expected_quantiles)
+        # Check for consistent hash-parameter set relationships
+        for set_name, parameter_set in TestMerge1.parameter_study.groupby(_set_coordinate_key):
+            assert parameter_set == TestMerge2.parameter_study.sel(parameter_sets=set_name)
+        # Self-consistency checks
+        assert list(TestMerge2._parameter_set_names.values()) == TestMerge2.parameter_study[_set_coordinate_key].values.tolist()
+        assert TestMerge2._parameter_set_hashes == TestMerge2.parameter_study[_hash_coordinate_key].values.tolist()
