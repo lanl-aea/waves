d7092448a1793fb51082331adbf89ce1d874a924
Kyle Brindley <kbrindley@lanl.gov>
2022-08-30 08:28:47 -0600

    TST: test merge behavior when the parameter study hasn't changed

--- waves/tests/test_cartesian_product.py
+++ waves/tests/test_cartesian_product.py
@@ -91,6 +91,13 @@ class TestCartesianProduct:
                   [1, 4.0, "a"],
                   [2, 4.0, "a"],
                   [1, 3.0, "a"]], dtype=object)),
+        'unchanged sets':
+            ({'parameter_1': [1, 2], 'parameter_2': [3.0], 'parameter_3': ['a']},
+             {'parameter_1': [1, 2], 'parameter_2': [3.0], 'parameter_3': ['a']},
+             # Ordered by md5 hash during Xarray merge operation. New tests must verify hash ordering.
+             numpy.array(
+                 [[2, 3.0, "a"],
+                  [1, 3.0, "a"]], dtype=object)),
     }
 
     @pytest.mark.unittest
