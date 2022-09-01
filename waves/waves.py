001c8c6f66f9079fa79c0e763062bb2357e8329d
Kyle Brindley <kbrindley@lanl.gov>
2022-09-01 09:43:27 -0600

    WIP: do nothing build subcommand

--- waves/waves.py
+++ waves/waves.py
@@ -9,14 +9,16 @@ from waves import __version__
 
 def main():
     """This is the main function that performs actions based on command line arguments.
-    
+
     :returns: return code
     """
     parser = get_parser()
     args = parser.parse_args()
-    
+
     if args.subcommand == 'docs':
         open_docs()
+    elif args.subcommand == 'build':
+        build()
     else:
         parser.print_help()
     return 0
@@ -24,7 +26,7 @@ def main():
 
 def get_parser():
     """Get parser object for command line options
-    
+
     :return: parser
     :rtype: ArgumentParser
     """
@@ -39,22 +41,34 @@ def get_parser():
         '-V', '--version',
         action='version',
         version=f'{_settings._project_name_short.upper()} {__version__}')
-    
+
     subparsers = main_parser.add_subparsers(
         help=f"Specify {_settings._project_name_short.lower()} sub-commands",
         title=f"{_settings._project_name_short} sub-commands",
-        description=f"Common {_settings._project_name_short.lower()} sub-commands to specify {_settings._project_name_short} usage",
-        dest='subcommand')  # so args.subcommand will contain the name of the subcommand called
-    
+        description=f"Common {_settings._project_name_short.lower()} sub-commands to specify " \
+                    f"{_settings._project_name_short} usage",
+        # So args.subcommand will contain the name of the subcommand called
+        dest='subcommand')
+
     docs_parser = argparse.ArgumentParser(add_help=False)
-    docs_parser = subparsers.add_parser('docs', help=f"Open the {_settings._project_name_short.upper()} HTML documentation",
-                                        description=f"Open the packaged {_settings._project_name_short.upper()} HTML documentation in the system default web browser",
-                                        parents=[docs_parser])
+    docs_parser = subparsers.add_parser('docs',
+            help=f"Open the {_settings._project_name_short.upper()} HTML documentation",
+            description=f"Open the packaged {_settings._project_name_short.upper()} HTML documentation in the  " \
+                         "system default web browser",
+            parents=[docs_parser])
     docs_parser.add_argument('-p', '--print-local-path',
                              action='version',
-                             version=f"{_settings._docs_directory}/index.html",  # unconventional usage of version argument. Usage here is to print the docs directory and exit
+                             # Unconventional usage of version argument.
+                             # Usage here is to print the docs directory and exit.
+                             version=f"{_settings._docs_directory}/index.html",
                              help=f"Print the path to the locally installed documentation index file. " \
                                   f"As an alternative to the docs sub-command, open index.html in a web browser.")
+
+    build_parser = argparse.ArgumentParser(add_help=False)
+    build_parser = subparsers.add_parser('build',
+        help=f"Thin SCons wrapper",
+        description=f"Thin SCons wrapper to programmatically re-run SCons until all targets are reported up-to-date.",
+        parents=[build_parser])
 
     return main_parser
 
@@ -62,6 +76,10 @@ def get_parser():
 def open_docs():
     webbrowser.open(f'{_settings._docs_directory}/index.html')
     return
+
+
+def build():
+    pass
 
 
 if __name__ == "__main__":
