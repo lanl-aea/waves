5fe89ff5fc69aba8dff95835d5c3504738867f0a
Thomas Phillip Roberts <tproberts@lanl.gov>
2022-09-01 07:55:01 -0600

    MAINT: remove white spaces per pep8

--- waves/builders.py
+++ waves/builders.py
@@ -150,13 +150,13 @@ def _abaqus_solver_emitter(target, source, env):
 def abaqus_solver(abaqus_program='abaqus', post_simulation=None):
     """Abaqus solver SCons builder
 
-    This builder requires that the root input file is the first source in the list. The builder returned by this 
-    function accepts all SCons Builder arguments and adds optional Builder keyword arguments ``job_name`` and 
-    ``abaqus_options``. If not specified ``job_name`` defaults to the root input file stem. The Builder emitter will 
+    This builder requires that the root input file is the first source in the list. The builder returned by this
+    function accepts all SCons Builder arguments and adds optional Builder keyword arguments ``job_name`` and
+    ``abaqus_options``. If not specified ``job_name`` defaults to the root input file stem. The Builder emitter will
     append common Abaqus output files as targets automatically from the ``job_name``, e.g. ``job_name.odb``.
 
-    The first target determines the working directory for the builder's action, as shown in the action code snippet 
-    below. The action changes the working directory to the first target's parent directory prior to executing the 
+    The first target determines the working directory for the builder's action, as shown in the action code snippet
+    below. The action changes the working directory to the first target's parent directory prior to executing the
     journal file.
 
     This builder is unique in that no targets are required. The Builder emitter will append the builder managed targets
