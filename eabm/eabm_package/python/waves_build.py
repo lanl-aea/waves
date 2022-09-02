de28b60b094c56995fcd44ef184e96cdb7005dfd
Kyle Brindley <kbrindley@lanl.gov>
2022-09-02 09:15:43 -0600

    WIP: merge minimal recursive study into WAVES-EABM structure

--- eabm/eabm_package/python/waves_build.py
+++ eabm/eabm_package/python/waves_build.py
@@ -1,2 +1,5 @@
-parameter_samples: [[1., 2., 3.], [1.1, 2.1, 3.1]]
-parameter_names: ['parameter_1', 'parameter_2', 'parameter_3']
+import numpy
+parameter_schema = {
+    "parameter_samples": numpy.array([[1., 1., 1.]]),
+    "parameter_names": ["parameter_1", "parameter_2", "parameter_3"]
+}
