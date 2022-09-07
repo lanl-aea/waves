c0337d4c3d993e5ad742c2257ba28a82a276342d
Kyle Brindley <kbrindley@lanl.gov>
2022-09-07 07:47:41 -0600

    MAINT: do not need to re-test the class behavior, which is tested by the class's unit/integration tests

--- waves/tests/test_parameter_study.py
+++ waves/tests/test_parameter_study.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import patch, call
 import sys
 
 import pytest
@@ -8,12 +8,14 @@ from waves import parameter_study
 parameter_study_args = {
     'cartesian product': (
         'cartesian_product',
-        {'parameter_1': [1, 2], 'parameter_2': ['a', 'b']}
+        {'parameter_1': [1, 2], 'parameter_2': ['a', 'b']},
+        'CartesianProduct'
     ),
     'custom study': (
         'custom_study',
         {'parameter_samples': [[1, 'a', 5.0], [2.0, 'b', 6]],
-         'parameter_names': ['height', 'prefix', 'index']}
+         'parameter_names': ['height', 'prefix', 'index']},
+        'CustomStudy'
     ),
     'latin hypercube': (
         'latin_hypercube',
@@ -26,16 +28,17 @@ parameter_study_args = {
            'distribution': 'skewnorm',
            'a': 4,
            'loc': 30,
-           'scale': 2}}
+           'scale': 2}},
+        'LatinHypercube'
     ),
 }
 
 
 @pytest.mark.integrationtest
-@pytest.mark.parametrize('subcommand, schema',
+@pytest.mark.parametrize('subcommand, schema, class_name',
                          parameter_study_args.values(),
                          ids=list(parameter_study_args.keys()))
-def test_parameter_study(subcommand, schema):
+def test_parameter_study(subcommand, schema, class_name):
     with patch('sys.argv', ['parameter_study.py', subcommand, '-h']), \
          pytest.raises(SystemExit) as pytest_exit:
         sys.exit(parameter_study.main())
@@ -44,6 +47,9 @@ def test_parameter_study(subcommand, schema):
     schema_file = 'dummy.file'
     with patch('sys.argv', ['parameter_study.py', subcommand, schema_file]), \
          patch('argparse.FileType'), patch('yaml.safe_load', return_value=schema), \
+         patch(f'waves.parameter_generators.{class_name}') as mock_generator, \
          pytest.raises(SystemExit) as pytest_exit:
         sys.exit(parameter_study.main())
+        assert mock_generator.assert_called_once()
+        assert mock_generator.method_calls == [call.generate(), call.write()]
     assert pytest_exit.value.code == 0
