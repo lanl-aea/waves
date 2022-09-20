c297c56134a381bdc54baaf6d6da99858178e514
Kyle Brindley <kbrindley@lanl.gov>
2022-09-20 14:43:24 -0600

    DOC: match CLI to implemented behavior

--- waves/waves.py
+++ waves/waves.py
@@ -99,8 +99,8 @@ def get_parser():
         parents=[quickstart_parser])
     quickstart_parser.add_argument("PROJECT_DIRECTORY",
         nargs="?",
-        help="Directory for new project template. Unless ``--overwrite`` is specified, the directory must not exist " \
-             "(default: PWD).",
+        help="Directory for new project template. Unless ``--overwrite`` is specified, the directory must not " \
+             "contain conflicting filenames. (default: PWD).",
         type=pathlib.Path,
         default=pathlib.Path().cwd())
     quickstart_parser.add_argument("--overwrite",
