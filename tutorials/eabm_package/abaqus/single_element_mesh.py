90237c4573bf120a567d1b76d4803a89004f02df
Kyle Brindley <kbrindley@lanl.gov>
2022-10-27 12:17:30 -0600

    WIP: fix the namespace for the abaqus utilities module

--- tutorials/eabm_package/abaqus/single_element_mesh.py
+++ tutorials/eabm_package/abaqus/single_element_mesh.py
@@ -66,7 +66,7 @@ def main(input_file, output_file, model_name, part_name, global_seed):
     p.Set(faces=faces, name='ALLNODES')
 
     model_object = abaqus.mdb.models[model_name]
-    abaqus_journal_utilities.export_mesh(model_object, part_name, output_file)
+    eabm_package.abaqus.abaqus_journal_utilities.export_mesh(model_object, part_name, output_file)
 
     abaqus.mdb.save()
 
