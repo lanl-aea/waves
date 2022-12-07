231b3726b44cb0a187e7a8bf6f9d5ca211684015
Kyle Brindley <kbrindley@lanl.gov>
2022-12-07 12:31:33 -0700

    TST: add alias list message tests

--- waves/tests/test_help_messages.py
+++ waves/tests/test_help_messages.py
@@ -6,11 +6,12 @@ import pytest
 
 from waves import builders
 
+
 @pytest.mark.unittest
 def test_default_targets_message():
     import SCons.Script  # Magic smoke that turns SCons.Defaults.DefaultEnvironment from a SCons.Environment.Base to SCons.Script.SConscript.SConsEnvironment
     import SCons.Defaults
-    env = SCons.Defaults.DefaultEnvironment() 
+    env = SCons.Defaults.DefaultEnvironment()
     env.Default()
     with patch("SCons.Environment.Environment.Help") as mock_help:
         builders.default_targets_message(env)
@@ -19,3 +20,17 @@ def test_default_targets_message():
     with patch("SCons.Environment.Environment.Help") as mock_help:
         builders.default_targets_message(env)
     mock_help.assert_called_once_with("\nDefault Targets:\n    dummy.target\n", append=True)
+
+
+@pytest.mark.unittest
+def test_alias_list_message():
+    import SCons.Script
+    import SCons.Defaults
+    env = SCons.Defaults.DefaultEnvironment()
+    with patch("SCons.Environment.Environment.Help") as mock_help:
+        builders.alias_list_message(env)
+    mock_help.assert_called_once_with("\nTarget Aliases:\n", append=True)
+    env.Alias("dummy_alias", "dummy.target")
+    with patch("SCons.Environment.Environment.Help") as mock_help:
+        builders.alias_list_message(env)
+    mock_help.assert_called_once_with("\nTarget Aliases:\n    dummy_alias\n", append=True)
