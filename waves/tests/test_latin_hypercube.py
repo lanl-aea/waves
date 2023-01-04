4beb3aaf4f45605cab1888ad626ed2fe97ae7ecf
Kyle Brindley <kbrindley@lanl.gov>
2023-01-04 12:22:39 -0700

    TST: test general scipy sampler merge feature using latin hypercube sampler

--- waves/tests/test_latin_hypercube.py
+++ waves/tests/test_latin_hypercube.py
@@ -100,10 +100,28 @@ class TestLatinHypercube:
                                  merge_test.values(),
                              ids=merge_test.keys())
     def test_merge(self, first_schema, second_schema, seed, expected_samples, expected_quantiles):
+        # LatinHypercube
         TestMerge1 = LatinHypercube(first_schema)
         TestMerge1.generate(kwargs={'seed': seed})
         with patch('xarray.open_dataset', return_value=TestMerge1.parameter_study):
             TestMerge2 = LatinHypercube(second_schema, previous_parameter_study='dummy_string')
+            TestMerge2.generate(kwargs={'seed': seed})
+        samples = TestMerge2._samples.astype(float)
+        quantiles = TestMerge2._quantiles.astype(float)
+        assert numpy.allclose(samples, expected_samples)
+        assert numpy.allclose(quantiles, expected_quantiles)
+        # Check for consistent hash-parameter set relationships
+        for set_name, parameter_set in TestMerge1.parameter_study.groupby(_set_coordinate_key):
+            assert parameter_set == TestMerge2.parameter_study.sel(parameter_sets=set_name)
+        # Self-consistency checks
+        assert list(TestMerge2._parameter_set_names.values()) == TestMerge2.parameter_study[_set_coordinate_key].values.tolist()
+        assert TestMerge2._parameter_set_hashes == TestMerge2.parameter_study[_hash_coordinate_key].values.tolist()
+
+        # ScipySampler 
+        TestMerge1 = ScipySampler("LatinHypercube", first_schema)
+        TestMerge1.generate(kwargs={'seed': seed})
+        with patch('xarray.open_dataset', return_value=TestMerge1.parameter_study):
+            TestMerge2 = ScipySampler("LatinHypercube", second_schema, previous_parameter_study='dummy_string')
             TestMerge2.generate(kwargs={'seed': seed})
         samples = TestMerge2._samples.astype(float)
         quantiles = TestMerge2._quantiles.astype(float)
