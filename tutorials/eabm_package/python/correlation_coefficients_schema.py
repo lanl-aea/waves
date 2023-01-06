a7bb767749c8709e4f8e82c614558d49c503f3fc
Kyle Brindley <kbrindley@lanl.gov>
2023-01-06 12:05:58 -0700

    WIP: syntax errors

--- tutorials/eabm_package/python/correlation_coefficients_schema.py
+++ tutorials/eabm_package/python/correlation_coefficients_schema.py
@@ -3,6 +3,7 @@ parameter_schema = {
     'problem': {
         'num_vars': 2,
         'names': ['width', 'height'],
-        'bounds': [[1., 0.1], [1., 0.1],
+        'bounds': [[1., 0.1], [1., 0.1]],
         'dists': ['norm', 'norm']
+    }
 }
