67e6b4d26fca3b51ede828ce169f057b4e03fae2
Kyle Brindley <kbrindley@lanl.gov>
2023-01-24 16:13:00 -0700

    TST: windows friendly root filing system expectations

--- waves/tests/test_builders.py
+++ waves/tests/test_builders.py
@@ -509,12 +509,12 @@ source_file = fs.File("/dummy.source")
 target_file = fs.File("/dummy.target")
 build_odb_extract_input = {
     "no kwargs": ([target_file], [source_file], {"abaqus_program": "NA"},
-                  [call(["/dummy.source"], "/dummy.target", output_type="h5", odb_report_args=None,
+                  [call([f"{root_fs}dummy.source"], f"{root_fs}dummy.target", output_type="h5", odb_report_args=None,
                        abaqus_command="NA", delete_report_file=False)]),
     "all kwargs": ([target_file], [source_file],
                    {"abaqus_program": "NA", "output_type": "different", "odb_report_args": "notnone",
                     "delete_report_file": True},
-                   [call(["/dummy.source"], "/dummy.target", output_type="different", odb_report_args="notnone",
+                   [call([f"{root_fs}dummy.source"], f"{root_fs}dummy.target", output_type="different", odb_report_args="notnone",
                         abaqus_command="NA", delete_report_file=True)])
 }
 
