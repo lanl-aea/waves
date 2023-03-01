a37b01094c7c3b69e23447774a853a935f165b67
Kyle Brindley <kbrindley@lanl.gov>
2023-03-01 13:48:21 -0700

    WIP: migrate quickstart tests to the new module

---
+++ waves/tests/test_fetch.py
@@ -0,0 +1,83 @@
+import pathlib
+from unittest.mock import patch
+
+import pytest
+
+from waves import fetch
+from waves import _settings
+
+_settings._installed_quickstart_directory
+
+
+@pytest.mark.unittest
+def test_quickstart():
+
+    # Dummy quickstart tree
+    quickstart_tree = [pathlib.Path("dummy.file")]
+
+    # Files in destination tree do not exist. Copy the quickstart file tree.
+    with patch("shutil.copyfile") as mock_copyfile, \
+         patch("pathlib.Path.mkdir") as mock_mkdir, \
+         patch("pathlib.Path.rglob", return_value=quickstart_tree), \
+         patch("pathlib.Path.relative_to", return_value=quickstart_tree[0]), \
+         patch("pathlib.Path.exists", side_effect=[True, False]):
+        return_code = fetch.recursive_copy(_settings._installed_quickstart_directory, "/dummy/path")
+        assert return_code == 0
+        mock_mkdir.assert_not_called()
+        mock_copyfile.assert_called_once()
+
+    # Files in destination tree do not exist, but dry run. Print quickstart file tree.
+    with patch("shutil.copyfile") as mock_copyfile, \
+         patch("pathlib.Path.mkdir") as mock_mkdir, \
+         patch("pathlib.Path.rglob", return_value=quickstart_tree), \
+         patch("pathlib.Path.relative_to", return_value=quickstart_tree[0]), \
+         patch("pathlib.Path.exists", side_effect=[True, False]):
+        return_code = fetch.recursive_copy(_settings._installed_quickstart_directory, "/dummy/path", dry_run=True)
+        assert return_code == 0
+        mock_mkdir.assert_not_called()
+        mock_copyfile.assert_not_called()
+
+    # Files in destination tree do exist. Don"t copy the quickstart file tree.
+    with patch("shutil.copyfile") as mock_copyfile, \
+         patch("pathlib.Path.mkdir") as mock_mkdir, \
+         patch("pathlib.Path.rglob", return_value=quickstart_tree), \
+         patch("pathlib.Path.relative_to", return_value=quickstart_tree[0]), \
+         patch("pathlib.Path.exists", side_effect=[True, True]):
+        return_code = fetch.recursive_copy(_settings._installed_quickstart_directory, "/dummy/path")
+        assert return_code != 0
+        mock_mkdir.assert_not_called()
+        mock_copyfile.assert_not_called()
+
+    # Files in destination tree do exist, but we want to overwrite contents. Copy the quickstart file tree.
+    with patch("shutil.copyfile") as mock_copyfile, \
+         patch("pathlib.Path.mkdir") as mock_mkdir, \
+         patch("pathlib.Path.rglob", return_value=quickstart_tree), \
+         patch("pathlib.Path.relative_to", return_value=quickstart_tree[0]), \
+         patch("pathlib.Path.exists", side_effect=[True, True]):
+        return_code = fetch.recursive_copy(_settings._installed_quickstart_directory, "/dummy/path", overwrite=True)
+        assert return_code == 0
+        mock_mkdir.assert_not_called()
+        mock_copyfile.assert_called_once()
+
+    # Files in destination tree do exist, but we want to overwrite contents and dry run. Print the quickstart file tree.
+    with patch("shutil.copyfile") as mock_copyfile, \
+         patch("pathlib.Path.mkdir") as mock_mkdir, \
+         patch("pathlib.Path.rglob", return_value=quickstart_tree), \
+         patch("pathlib.Path.relative_to", return_value=quickstart_tree[0]), \
+         patch("pathlib.Path.exists", side_effect=[True, True]):
+        return_code = fetch.recursive_copy(_settings._installed_quickstart_directory, "/dummy/path", overwrite=True, dry_run=True)
+        assert return_code == 0
+        mock_mkdir.assert_not_called()
+        mock_copyfile.assert_not_called()
+
+    # Test the "unreachable" exit code used as a sign-of-life that the installed package structure assumptions in
+    # _settings.py are correct.
+    with patch("shutil.copyfile") as mock_copyfile, \
+         patch("pathlib.Path.mkdir") as mock_mkdir, \
+         patch("pathlib.Path.rglob", return_value=quickstart_tree), \
+         patch("pathlib.Path.relative_to", return_value=quickstart_tree[0]), \
+         patch("pathlib.Path.exists", side_effect=[False, False]):
+        return_code = fetch.recursive_copy(_settings._installed_quickstart_directory, "/dummy/path")
+        assert return_code != 0
+        mock_mkdir.assert_not_called()
+        mock_copyfile.assert_not_called()
