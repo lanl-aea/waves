28fbb87a57089b3924bde7b85e3ac9a71d9ee6d8
Kyle Brindley <kbrindley@lanl.gov>
2023-03-01 13:53:38 -0700

    MAINT: begin refactoring variable names to match enhanced behavior

--- waves/fetch.py
+++ waves/fetch.py
@@ -5,20 +5,19 @@ import pathlib
 from waves import _settings
 
 
-def recursive_copy(source, directory, overwrite=False, dry_run=False):
-    """Copy project quickstart template files into directory
+def recursive_copy(source, destination, overwrite=False, dry_run=False):
+    """Recursively copy source directory into destination directory
 
-    Copies from the project quickstart tree to the directory. If files exist, report conflicting files and exit with a
-    non-zero return code unless overwrite is specified.
+    If files exist, report conflicting files and exit with a non-zero return code unless overwrite is specified.
 
     :param str source: String or pathlike object for the source directory
-    :param str directory: String or pathlike object for the new template directory destination
-    :param bool overwrite: Boolean to overwrite any existing files in directory destination
+    :param str destination: String or pathlike object for the destination directory
+    :param bool overwrite: Boolean to overwrite any existing files in destination directory
     :param bool dry_run: Print the template destination tree and exit
     """
     # Gather source and destination lists
     source = pathlib.Path(source).resolve()
-    directory = pathlib.Path(directory).resolve()
+    destination = pathlib.Path(destination).resolve()
     if not source.exists():
         # This should only be reached if the package installation structure doesn't match the assumptions in
         # _settings.py. It is used by the Conda build tests as a sign-of-life that the assumptions are correct.
@@ -33,30 +32,29 @@ def recursive_copy(source, directory, overwrite=False, dry_run=False):
         print(f"Did not find any quickstart files or directories in {source}",
               file=sys.stderr)
         return 1
-    directory_dirs = [directory / path.relative_to(source)
+    destination_dirs = [destination / path.relative_to(source)
                       for path in quickstart_dirs]
-    directory_files = [directory / path.relative_to(source)
+    destination_files = [destination / path.relative_to(source)
                        for path in quickstart_files]
-    existing_files = [path for path in directory_files if path.exists()]
+    existing_files = [path for path in destination_files if path.exists()]
 
     # User I/O
     print(f"{_settings._project_name_short} Quickstart", file=sys.stdout)
-    print(f"Project root path: '{directory}'", file=sys.stdout)
+    print(f"Project root path: '{destination}'", file=sys.stdout)
     if not overwrite and existing_files:
-        print(f"Found conflicting files in destination '{directory}':", file=sys.stderr)
+        print(f"Found conflicting files in destination '{destination}':", file=sys.stderr)
         for path in existing_files:
             print(f"\t{path}", file=sys.stderr)
         return 2
     if dry_run:
         print("Files to create:")
-        for path in directory_files:
+        for path in destination_files:
             print(f"\t{path}", file=sys.stdout)
         return 0
 
     # Do the work
-    for path in directory_dirs:
+    for path in destination_dirs:
         path.mkdir(parents=True, exist_ok=True)
-    for source, destination in zip(quickstart_files, directory_files):
+    for source, destination in zip(quickstart_files, destination_files):
         shutil.copyfile(source, destination)
     return 0
-
