745554c907a56d5a9dced9871601bb29bb25be39
Kyle Brindley <kbrindley@lanl.gov>
2023-03-06 12:20:48 -0700

    FEAT: include the tutorial files in the conda package

--- package_documentation.py
+++ package_documentation.py
@@ -11,12 +11,14 @@ html_path_external = pathlib.Path("build/docs/html").resolve()
 html_path_internal = pathlib.Path("build/docs/html-internal").resolve()
 html_path = html_path_external if html_path_external.exists() else html_path_internal
 quickstart_path = pathlib.Path("quickstart").resolve()
+tutorials_path = pathlib.Path("tutorials").resolve()
 
 new_paths = [
     (prefix / "share/man/man1", man_path),
     (prefix / "man/man1", man_path),
     (sp_dir / pkg_name / "docs", html_path),
-    (sp_dir / pkg_name / "quickstart", quickstart_path)
+    (sp_dir / pkg_name / "quickstart", quickstart_path),
+    (sp_dir / pkg_name / "tutorials", tutorials_path),
 ]
 for destination, source in new_paths:
     assert source.exists()
