da478f284493ff5c9d26c6eb7ea7cfe5720a5fdb
Kyle Brindley <kbrindley@lanl.gov>
2023-03-06 10:18:12 -0700

    WIP: make the quickstart settings a subset of the fetch settings

--- waves/waves.py
+++ waves/waves.py
@@ -21,10 +21,15 @@ def main():
     elif args.subcommand == 'build':
         return_code = build(args.TARGET, scons_args=unknown, max_iterations=args.max_iterations,
                             working_directory=args.working_directory, git_clone_directory=args.git_clone_directory)
+    elif args.subcommand == 'fetch':
+        root_directory = _settings._installed_quickstart_directory.parent
+        relative_paths = [_settings._fetch_subdirectories]
+        return_code = fetch(args.subcommand, root_directory, relative_paths, args.destination, requested_paths=args.FILE,
+                            overwrite=args.overwrite, dry_run=args.dry_run, print_available=args.print_available)
     elif args.subcommand == 'quickstart':
         root_directory = _settings._installed_quickstart_directory.parent
         relative_paths = [_settings._installed_quickstart_directory.name]
-        return_code = fetch(root_directory, relative_paths, args.destination,
+        return_code = fetch(args.subcommand, root_directory, relative_paths, args.destination,
                             overwrite=args.overwrite, dry_run=args.dry_run)
     elif args.subcommand == 'visualize':
         return_code = visualization(target=args.TARGET, output_file=args.output_file,
