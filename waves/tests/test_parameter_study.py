1fc555c15238a9e4853d2b37bae9c82e061a2397
Kyle Brindley <kbrindley@lanl.gov>
2023-03-17 14:39:47 -0600

    WIP: test successful exit. wip to check the generator class

--- waves/tests/test_parameter_study.py
+++ waves/tests/test_parameter_study.py
@@ -30,17 +30,21 @@ parameter_study_args = {
                          parameter_study_args.values(),
                          ids=list(parameter_study_args.keys()))
 def test_parameter_study(subcommand, class_name):
-    with patch('sys.argv', ['parameter_study.py', subcommand, '-h']), \
-         pytest.raises(SystemExit) as pytest_exit:
-        sys.exit(parameter_study.main())
-    assert pytest_exit.value.code == 0
+    # Should not raise
+    with patch('sys.argv', ['parameter_study.py', subcommand, '-h']):
+        try:
+            parameter_study.main()
+        except SystemExit as err:
+            assert err.code == 0
 
     schema_file = 'dummy.file'
     with patch('sys.argv', ['parameter_study.py', subcommand, schema_file]), \
          patch('argparse.FileType'), patch('yaml.safe_load'), \
-         patch(f'waves.parameter_generators.{class_name}') as mock_generator, \
-         pytest.raises(SystemExit) as pytest_exit:
-        sys.exit(parameter_study.main())
-        assert mock_generator.assert_called_once()
-        assert mock_generator.method_calls == [call.write()]
-    assert pytest_exit.value.code == 0
+         patch(f'waves.parameter_generators.{class_name}') as mock_generator:
+        try:
+            parameter_study.main()
+        except SystemExit:
+            pass
+        finally:
+            mock_generator.assert_called_once()
+            assert mock_generator.method_calls == [call.write()]
