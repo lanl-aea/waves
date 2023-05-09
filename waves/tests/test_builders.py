be460816b4c6f7a1b021e23855b10e94e9d6b052
Kyle Brindley <kbrindley@lanl.gov>
2023-05-09 11:17:06 -0600

    TST: add part quoting tests

--- waves/tests/test_builders.py
+++ waves/tests/test_builders.py
@@ -121,6 +121,26 @@ def test_substitution_syntax(substitution_dictionary, keyword_arguments, expecte
     assert output_dictionary == expected_dictionary
 
 
+quote_spaces_in_path_input = {
+    "string, no spaces": (
+        "/path/without_space/executable",
+        pathlib.Path("/path/without_space/executable")
+    ),
+    "string, spaces": (
+        "/path/with space/executable",
+        pathlib.Path("/path/\"with space\"/executable")
+    ),
+}
+
+
+@pytest.mark.unittest
+@pytest.mark.parametrize("path, expected",
+                         quote_spaces_in_path_input.values(),
+                         ids=quote_spaces_in_path_input.keys())
+def test_quote_spaces_in_path(path, expected):
+    assert builders._quote_spaces_in_path(path) == expected
+
+
 find_program_input = {
     "string": (
         "dummy",
