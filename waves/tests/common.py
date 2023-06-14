b3a23994cc055f9d42cae493a7fcb525784a1412
Sergio Cordova <sergioc@lanl.gov>
2023-06-14 10:06:27 -0600

    Added docstrings.

--- waves/tests/common.py
+++ waves/tests/common.py
@@ -2,18 +2,38 @@ from waves._settings import _hash_coordinate_key, _set_coordinate_key
 from unittest.mock import patch
 
 
-def consistent_hash_parameter_check(test_merge1, test_merge2):
-    for set_name, parameter_set in test_merge1.parameter_study.groupby(_set_coordinate_key):
-        assert parameter_set == test_merge2.parameter_study.sel(parameter_sets=set_name)
+def consistent_hash_parameter_check(original_study, merged_study):
+    """Assert that the merged parameter study data matches the original parameter study. 
+
+    :param Union[CartesianProduct, SobolSequence, ScipySampler, SALibSampler] original_study: Original sampler object
+    :param Union[CartesianProduct, SobolSequence, ScipySampler, SALibSampler] merged_study: Merged sampler object
+    """
+    for set_name, parameter_set in original_study.parameter_study.groupby(_set_coordinate_key):
+        assert parameter_set == merged_study.parameter_study.sel(parameter_sets=set_name)
 
 
-def self_consistency_checks(test_merge):
-    assert list(test_merge._parameter_set_names.values()) == test_merge.parameter_study[
+def self_consistency_checks(merged_study):
+    """Assert that the merged parameter set data is consistent throughout the sampler object. 
+
+    :param Union[CartesianProduct, SobolSequence, ScipySampler, SALibSampler] merged_study: Sampler object
+    """
+    assert list(merged_study._parameter_set_names.values()) == merged_study.parameter_study[
         _set_coordinate_key].values.tolist()
-    assert test_merge._parameter_set_hashes == test_merge.parameter_study[_hash_coordinate_key].values.tolist()
+    assert merged_study._parameter_set_hashes == merged_study.parameter_study[_hash_coordinate_key].values.tolist()
 
 
 def merge_samplers(sampler_class, first_schema, second_schema, kwargs, sampler=None):
+    """Return sampler objects based on the provided schemas and sampler class.
+
+    :param Union[CartesianProduct, SobolSequence, ScipySampler, SALibSampler] sampler_class: Class of the study objects
+    :param dict first_schema: Dictionary containing parameter study data
+    :param dict second_schema: Dictionary containing parameter study data
+    :param dict kwargs: Dictionary containing keyword arguments
+    :param str sampler: Optional sampler type
+
+    :return: original_study, merged_study
+    :rtype: (Union[CartesianProduct, SobolSequence, ScipySampler, SALibSampler], Union[CartesianProduct, SobolSequence, ScipySampler, SALibSampler])
+    """
     if sampler:
         original_study = sampler_class(sampler, first_schema, **kwargs)
     else:
