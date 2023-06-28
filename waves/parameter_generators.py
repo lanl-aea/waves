3a434b8e1633a6a0ae5343fa4de7f4d88aebb294
Sergio Cordova <sergioc@lanl.gov>
2023-06-28 11:54:10 -0600

    Renamed variables

--- waves/parameter_generators.py
+++ waves/parameter_generators.py
@@ -243,20 +243,20 @@ class _ParameterGenerator(ABC):
                     else:
                         self._conditionally_write_dataset(parameter_set_file, parameter_set)
 
-    def _conditionally_write_dataset(self, previous_parameter_study, parameter_study):
+    def _conditionally_write_dataset(self, existing_parameter_study, parameter_study):
         """Write NetCDF file over previous study if the datasets have changed or self.overwrite is True
 
-        :param str previous_parameter_study: A relative or absolute file path to a previously created parameter
+        :param str existing_parameter_study: A relative or absolute file path to a previously created parameter
             study Xarray Dataset
         :param xarray.Dataset parameter_study: Parameter study xarray data
         """
         write = True
-        if not self.overwrite and pathlib.Path(previous_parameter_study).is_file():
-            with xarray.open_dataset(previous_parameter_study, engine='h5netcdf') as existing_dataset:
+        if not self.overwrite and pathlib.Path(existing_parameter_study).is_file():
+            with xarray.open_dataset(existing_parameter_study, engine='h5netcdf') as existing_dataset:
                 if parameter_study.equals(existing_dataset):
                     write = False
         if write:
-            parameter_study.to_netcdf(path=previous_parameter_study, mode='w', format="NETCDF4", engine='h5netcdf')
+            parameter_study.to_netcdf(path=existing_parameter_study, mode='w', format="NETCDF4", engine='h5netcdf')
 
     def _write_yaml(self, parameter_set_files):
         """Write YAML formatted output to STDOUT, separate set files, or a single file
@@ -306,9 +306,9 @@ class _ParameterGenerator(ABC):
         """
         write = True
         if not self.overwrite and pathlib.Path(output_file).is_file():
-            with open(output_file, 'r') as current_file:
-                current_yaml_object = yaml.safe_load(current_file)
-                if current_yaml_object == new_parameter_dictionary:
+            with open(output_file, 'r') as existing_file:
+                existing_yaml_object = yaml.safe_load(existing_file)
+                if existing_yaml_object == new_parameter_dictionary:
                     write = False
         if write:
             with open(output_file, 'w') as outfile:
