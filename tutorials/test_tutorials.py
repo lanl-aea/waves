9af5f84ca018754f3631a97bc0eeb0f825680321
Kyle Brindley <kbrindley@lanl.gov>
2023-06-30 12:44:03 -0600

    WIP: organize match gitlab ci file

--- tutorials/test_tutorials.py
+++ tutorials/test_tutorials.py
@@ -6,23 +6,24 @@ import subprocess
 import pytest
 
 
+env = os.environ.copy()
 try:
     import waves
 except ModuleNotFoundError:
     # TODO: resolve from package? SConstruct?
     package_parent_path = str(pathlib.Path(".").resolve().parent)
-    env = os.environ.copy()
     key = "PYTHONPATH"
     if key in env:
         env[key] = f"{package_parent_path}:{env[key]}"
     else:
         env[key] = package_parent_path
 
+@pytest.mark.systemtest
 @pytest.mark.parametrize("command", [
-    ["scons", ".", "--sconstruct=tutorial_00_SConstruct", "--keep-going", "--unconditional-build"],
     ["scons", ".", "--sconstruct=scons_quickstart_SConstruct", "--keep-going"],
     ["scons", ".", "--sconstruct=scons_multiactiontask_SConstruct", "--keep-going"],
     ["scons", ".", "--sconstruct=waves_quickstart_SConstruct", "--keep-going"],
+    ["scons", ".", "--sconstruct=tutorial_00_SConstruct", "--keep-going", "--unconditional-build"],
 ])
 def test_run_tutorial(command):
     result = subprocess.check_output(command, env=env).decode('utf-8')
