119f4396faa483fe4374c640b961c469c99aaf8f
Sergio Cordova <sergioc@lanl.gov>
2023-07-11 12:57:28 -0600

    Moved scanners to builders.py module

--- waves/builders.py
+++ waves/builders.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python
 
 import pathlib
+import re
 
 import SCons.Defaults
 import SCons.Builder
 import SCons.Environment
 import SCons.Node
+import SCons.Scanner
 
 from waves.abaqus import odb_extract
 from waves._settings import _abaqus_environment_extension
@@ -934,3 +936,40 @@ def sbatch(sbatch_program="sbatch", post_action=None):
         action=action,
         emitter=_first_target_emitter)
     return sbatch_builder
+
+
+def abaqus_implicit_scanner():
+    """Abaqus implicit dependency scanner
+
+    Custom Scons scanner that searches for ``INCLUDE`` keyword inside of ``.inp`` files.
+
+    :return: Abaqus implicit dependency Scanner
+    :rtype: Scons.Scanner.Scanner
+    """
+    return custom_scanner(r'^\*INCLUDE,\s*input=(.+)$', ['.inp'])
+
+
+def custom_scanner(pattern, suffixes):
+    """Custom Scons scanner
+
+    constructs a scanner object based on a regular expression pattern. Will only search for files matching the list of
+    suffixes provided.
+
+    :param str pattern: Regular expression pattern.
+    :param list suffixes: List of suffixes of files to search
+
+    :return: Custom Scons scanner
+    :rtype: Scons.Scanner.Scanner
+    """
+    inp_pattern = re.compile(pattern, re.M | re.I)
+
+    def inp_scan(node, env, path):
+        contents = node.get_text_contents()
+        includes = inp_pattern.findall(contents)
+        return includes
+
+    def inp_only(node_list):
+        return [node for node in node_list if node.path.endswith(tuple(suffixes))]
+
+    inp_scanner = SCons.Scanner.Scanner(function=inp_scan, skeys=suffixes, recursive=inp_only)
+    return inp_scanner
