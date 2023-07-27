75cb0edfca002e474fe364afddb7df92d98eb845
Kyle Brindley <kbrindley@lanl.gov>
2023-07-27 10:37:37 -0600

    DOC: inline dev note with module deprecation timing and gitlab issue

--- waves/__init__.py
+++ waves/__init__.py
@@ -37,6 +37,7 @@ from importlib.metadata import version, PackageNotFoundError
 
 from waves import scons
 # TODO: Remove the builders module for v1.0
+# https://re-git.lanl.gov/aea/python-projects/waves/-/issues/511
 from waves import builders
 from waves import parameter_generators
 
--- waves/builders.py
+++ waves/builders.py
@@ -2,6 +2,8 @@
 import warnings
 from waves.scons import *
 
+# TODO: Remove the builders module for v1.0
+# https://re-git.lanl.gov/aea/python-projects/waves/-/issues/511
 warnings.simplefilter('always', DeprecationWarning)
 message = "The 'waves.builders' module will be deprecated in a future version. Use the 'waves.scons' module instead"
 warnings.warn(message, DeprecationWarning)
