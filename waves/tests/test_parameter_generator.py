2c425130336f3f527f03f20e9beb0a2718e767a9
Sergio Cordova <sergioc@lanl.gov>
2023-08-17 09:54:00 -0600

    Updated tests.

--- waves/tests/test_parameter_generator.py
+++ waves/tests/test_parameter_generator.py
@@ -89,29 +89,28 @@ class TestParameterGenerator:
                                                      set_name_template=set_template, **kwargs)
         assert list(TemplateGenerator._parameter_set_names.values()) == expected
 
-    init_write_stdout = {# schema, template, overwrite, dryrun, debug,         is_file,  sets, stdout_calls
-        'no-template-1': (     {},     None,     False,  False, False,          [False],    1,            1),
-        'no-template-2': (     {},     None,      True,  False, False,          [False],    1,            1),
-        'no-template-3': (     {},     None,     False,   True, False,   [False, False],    2,            1),
-        'no-template-4': (     {},     None,     False,  False, False,   [ True,  True],    2,            1),
-        'dryrun-1':      (     {},    'out',     False,   True, False,          [False],    1,            1),
-        'dryrun-2':      (     {},    'out',      True,   True, False,          [False],    1,            1),
-        'dryrun-3':      (     {},    'out',      True,   True, False,   [ True, False],    2,            2),
-        'dryrun-4':      (     {},    'out',     False,   True, False,   [False,  True],    1,            1),
+    init_write_stdout = {# schema, template, overwrite, dryrun,         is_file,  sets, stdout_calls
+        'no-template-1': (     {},     None,     False,  False,          [False],    1,            1),
+        'no-template-2': (     {},     None,      True,  False,          [False],    1,            1),
+        'no-template-3': (     {},     None,     False,   True,   [False, False],    2,            1),
+        'no-template-4': (     {},     None,     False,  False,   [ True,  True],    2,            1),
+        'dryrun-1':      (     {},    'out',     False,   True,          [False],    1,            1),
+        'dryrun-2':      (     {},    'out',      True,   True,          [False],    1,            1),
+        'dryrun-3':      (     {},    'out',      True,   True,   [ True, False],    2,            2),
+        'dryrun-4':      (     {},    'out',     False,   True,   [False,  True],    1,            1),
     }
 
     @pytest.mark.unittest
-    @pytest.mark.parametrize('schema, template, overwrite, dryrun, debug, is_file, sets, stdout_calls',
+    @pytest.mark.parametrize('schema, template, overwrite, dryrun, is_file, sets, stdout_calls',
                                  init_write_stdout.values(),
                              ids=init_write_stdout.keys())
-    def test_write_to_stdout(self, schema, template, overwrite, dryrun, debug, is_file, sets, stdout_calls):
+    def test_write_to_stdout(self, schema, template, overwrite, dryrun, is_file, sets, stdout_calls):
         """Check for conditions that should result in calls to stdout
 
         :param str schema: placeholder string standing in for the schema read from an input file
         :param str template: user supplied string to be used as a template for output file names
         :param bool overwrite: overwrite existing files
         :param bool dryrun: skip file write, but show file name and associated contents that would ahve been written
-        :param bool debug: print cli debugging information and exit
         :param bool is_file: test specific argument mocks output for pathlib.Path().is_file()
         :param int sets: test specific argument for the number of sets to build for the test
         :param int stdout_calls: number of calls to stdout. Should only differ from set count when no template is
@@ -119,7 +118,7 @@ class TestParameterGenerator:
         """
         kwargs = {"sets": sets}
         WriteParameterGenerator = NoQuantilesGenerator(schema, output_file_template=template, output_file_type='yaml',
-                                                       overwrite=overwrite, dryrun=dryrun, debug=debug, **kwargs)
+                                                       overwrite=overwrite, dryrun=dryrun, **kwargs)
         with patch('waves.parameter_generators._ParameterGenerator._write_meta'), \
              patch('builtins.open', mock_open()) as mock_file, \
              patch('sys.stdout.write') as stdout_write, \
@@ -131,35 +130,34 @@ class TestParameterGenerator:
             xarray_to_netcdf.assert_not_called()
             assert stdout_write.call_count == stdout_calls
 
-    init_write_files = {# schema, template, overwrite, dryrun, debug,          is_file, sets, files
-        'template-1':  (      {},    'out',     False,  False, False,          [False],    1,     1),
-        'template-2':  (      {},    'out',     False,  False, False,   [False, False],    2,     2),
-        'template-3':  (      {},    'out',     False,  False, False,   [ True,  True],    2,     0),
-        'template-4':  (      {},    'out',     False,  False, False,   [ True, False],    2,     1),
-        'overwrite-2': (      {},    'out',      True,  False, False,   [False, False],    2,     2),
-        'overwrite-3': (      {},    'out',      True,  False, False,   [ True,  True],    2,     2),
-        'overwrite-4': (      {},    'out',      True,  False, False,   [ True, False],    2,     2),
+    init_write_files = {# schema, template, overwrite, dryrun,          is_file, sets, files
+        'template-1':  (      {},    'out',     False,  False,          [False],    1,     1),
+        'template-2':  (      {},    'out',     False,  False,   [False, False],    2,     2),
+        'template-3':  (      {},    'out',     False,  False,   [ True,  True],    2,     0),
+        'template-4':  (      {},    'out',     False,  False,   [ True, False],    2,     1),
+        'overwrite-2': (      {},    'out',      True,  False,   [False, False],    2,     2),
+        'overwrite-3': (      {},    'out',      True,  False,   [ True,  True],    2,     2),
+        'overwrite-4': (      {},    'out',      True,  False,   [ True, False],    2,     2),
     }
 
     @pytest.mark.unittest
-    @pytest.mark.parametrize('schema, template, overwrite, dryrun, debug, is_file, sets, files',
+    @pytest.mark.parametrize('schema, template, overwrite, dryrun, is_file, sets, files',
                                  init_write_files.values(),
                              ids=init_write_files.keys())
-    def test_write_yaml(self, schema, template, overwrite, dryrun, debug, is_file, sets, files):
+    def test_write_yaml(self, schema, template, overwrite, dryrun, is_file, sets, files):
         """Check for conditions that should result in calls to builtins.open
 
         :param str schema: placeholder string standing in for the schema read from an input file
         :param str template: user supplied string to be used as a template for output file names
         :param bool overwrite: overwrite existing files
         :param bool dryrun: skip file write, but show file name and associated contents that would ahve been written
-        :param bool debug: print cli debugging information and exit
         :param list is_file: test specific argument mocks changing output for pathlib.Path().is_file() repeat calls
         :param int sets: test specific argument for the number of sets to build for the test
         :param int files: integer number of files that should be written
         """
         kwargs = {"sets": sets}
         WriteParameterGenerator = NoQuantilesGenerator(schema, output_file_template=template, output_file_type='yaml',
-                                                       overwrite=overwrite, dryrun=dryrun, debug=debug, **kwargs)
+                                                       overwrite=overwrite, dryrun=dryrun, **kwargs)
         with patch('waves.parameter_generators._ParameterGenerator._write_meta'), \
              patch('waves.parameter_generators._ParameterGenerator._conditionally_write_yaml') as mock_file, \
              patch('sys.stdout.write') as stdout_write, \
@@ -171,24 +169,23 @@ class TestParameterGenerator:
             assert mock_file.call_count == files
 
     @pytest.mark.unittest
-    @pytest.mark.parametrize('schema, template, overwrite, dryrun, debug, is_file, sets, files',
+    @pytest.mark.parametrize('schema, template, overwrite, dryrun, is_file, sets, files',
                                  init_write_files.values(),
                              ids=init_write_files.keys())
-    def test_write_dataset(self, schema, template, overwrite, dryrun, debug, is_file, sets, files):
+    def test_write_dataset(self, schema, template, overwrite, dryrun, is_file, sets, files):
         """Check for conditions that should result in calls to _ParameterGenerator._write_netcdf
 
         :param str schema: placeholder string standing in for the schema read from an input file
         :param str template: user supplied string to be used as a template for output file names
         :param bool overwrite: overwrite existing files
         :param bool dryrun: skip file write, but show file name and associated contents that would ahve been written
-        :param bool debug: print cli debugging information and exit
         :param list is_file: test specific argument mocks changing output for pathlib.Path().is_file() repeat calls
         :param int sets: test specific argument for the number of sets to build for the test
         :param int files: integer number of files that should be written
         """
         kwargs = {"sets": sets}
         WriteParameterGenerator = NoQuantilesGenerator(schema, output_file_template=template, output_file_type='h5',
-                                                       overwrite=overwrite, dryrun=dryrun, debug=debug, **kwargs)
+                                                       overwrite=overwrite, dryrun=dryrun, **kwargs)
 
         with patch('waves.parameter_generators._ParameterGenerator._write_meta'), \
              patch('builtins.open', mock_open()) as mock_file, \
