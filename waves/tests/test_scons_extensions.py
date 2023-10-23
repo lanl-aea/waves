cfd3a9a053af50e208fee3e270350f8b388bf346
Sergio Cordova <sergioc@lanl.gov>
2023-10-23 14:07:28 -0600

    Added utilities, updated tests.

--- waves/tests/test_scons_extensions.py
+++ waves/tests/test_scons_extensions.py
@@ -250,46 +250,6 @@ def test_substitution_syntax(substitution_dictionary, keyword_arguments, expecte
     assert output_dictionary == expected_dictionary
 
 
-quote_spaces_in_path_input = {
-    "string, no spaces": (
-        "/path/without_space/executable",
-        pathlib.Path("/path/without_space/executable")
-    ),
-    "string, spaces": (
-        "/path/with space/executable",
-        pathlib.Path("/path/\"with space\"/executable")
-    ),
-    "pathlib, no spaces": (
-        pathlib.Path("/path/without_space/executable"),
-        pathlib.Path("/path/without_space/executable")
-    ),
-    "pathlib, spaces": (
-        pathlib.Path("/path/with space/executable"),
-        pathlib.Path("/path/\"with space\"/executable")
-    ),
-    "space in root": (
-        pathlib.Path("/path space/with space/executable"),
-        pathlib.Path("/\"path space\"/\"with space\"/executable")
-    ),
-    "relative path": (
-        pathlib.Path("path space/without_space/executable"),
-        pathlib.Path("\"path space\"/without_space/executable")
-    ),
-    "space in executable": (
-        pathlib.Path("path/without_space/executable space"),
-        pathlib.Path("path/without_space/\"executable space\"")
-    )
-}
-
-
-@pytest.mark.unittest
-@pytest.mark.parametrize("path, expected",
-                         quote_spaces_in_path_input.values(),
-                         ids=quote_spaces_in_path_input.keys())
-def test_quote_spaces_in_path(path, expected):
-    assert scons_extensions._quote_spaces_in_path(path) == expected
-
-
 return_environment = {
     "no newlines": (b"thing1=a\x00thing2=b", {"thing1": "a", "thing2": "b"}),
     "newlines": (b"thing1=a\nnewline\x00thing2=b", {"thing1": "a\nnewline", "thing2": "b"})
