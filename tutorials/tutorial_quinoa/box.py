64e412972473b7f8c7404a50ffe21ee21f418f01
Kyle Brindley <kbrindley@lanl.gov>
2023-10-27 08:29:22 -0600

    WIP: initial functioning project from practice repo: https://re-git.lanl.gov/aea/models-and-simulations/waves-quinoa

---
+++ tutorials/tutorial_quinoa/box.py
@@ -0,0 +1,79 @@
+import sys
+import argparse
+import pathlib
+
+import cubit
+
+
+def main(output_file, xlength, ylength, zlength):
+    """Write brick geometry with specified dimensions"""
+    cubit.init(['cubit', '-noecho', '-nojournal', '-nographics', '-batch'])
+    cubit.cmd('new')
+    cubit.cmd('reset')
+
+    cubit_file = pathlib.Path(output_file).with_suffix(".cub")
+    exodus_file = cubit_file.with_suffix(".exo")
+
+    # Geometry
+    cubit.cmd(f"brick x {xlength} y {ylength} z {zlength}")
+    cubit.cmd(f"move volume 1 x {xlength*.5} y {ylength*.5} z {zlength*.5}")
+    cubit.cmd(f"webcut volume 1 with plane xplane offset {xlength*.5} noimprint nomerge")
+    cubit.cmd(f"merge volume 1 2")
+
+    # Sets (partition)
+    cubit.cmd("sideset 1 add surface 8 15")
+    cubit.cmd("sideset 2 add surface 9 16")
+    cubit.cmd("sideset 3 add surface 10 14")
+    cubit.cmd("sideset 4 add surface 4")
+    cubit.cmd("sideset 5 add surface 11 13")
+    cubit.cmd("sideset 6 add surface 6")
+
+    # Mesh
+    cubit.cmd("Trimesher surface gradation 1.05")
+    cubit.cmd("Trimesher volume gradation 1.05")
+
+    cubit.cmd("surface 6 7 8 9 10 11 13 14 15 16 Scheme TriMesh geometry approximation angle 10")
+    cubit.cmd("surface 13 14 15 16  size 0.065")
+    cubit.cmd("surface 4 size 0.09")
+    cubit.cmd("surface 7 size .09")
+    cubit.cmd("surface 8 9 10 11 size 0.45")
+    cubit.cmd("surface 6 size 0.09")
+
+    cubit.cmd("mesh surface 6 7 8 9 10 11 13 14 15 16")
+
+    cubit.cmd("volume 1 2 Scheme Tetmesh proximity layers off geometry approximation angle 10")
+    cubit.cmd("volume 1 2 Tetmesh growth_factor 1.001")
+    cubit.cmd("volume 1 size 0.065")
+    cubit.cmd("volume 2 size 0.065")
+
+    cubit.cmd("mesh volume 1 2")
+    cubit.cmd("block 1 add volume 1 2")
+
+    cubit.cmd("set exodus netcdf4 off")
+    cubit.cmd("set large exodus file on")
+    cubit.cmd(f"export Genesis '{exodus_file}' dimension 3 block 1  overwrite")
+    cubit.cmd(f"save as '{cubit_file}' overwrite")
+
+
+def get_parser():
+    script_name = pathlib.Path(__file__)
+
+    prog = f"python {script_name.name} "
+    cli_description = "Write brick geometry ``output_file``.cub with specified dimensions"
+    parser = argparse.ArgumentParser(description=cli_description, prog=prog)
+    parser.add_argument('--output-file', type=str, required=True)
+    parser.add_argument('--xlength', type=float, required=True)
+    parser.add_argument('--ylength', type=float, required=True)
+    parser.add_argument('--zlength', type=float, required=True)
+    return parser
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args, unknown = parser.parse_known_args()
+    sys.exit(main(
+        output_file=args.output_file,
+        xlength=args.xlength,
+        ylength=args.ylength,
+        zlength=args.zlength
+    ))
