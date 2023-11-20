fb8c3ad65801509aed5cc6a5f9b844ceb69435e1
Kyle Brindley <kbrindley@lanl.gov>
2023-11-20 07:56:03 -0700

    WIP: add Python 3 search/find_commands and cubit bin check from TurboTurtle. Differ from SCons extensions in that these don't use SCons search methods. Probably worth keeping these utilities separate for now since the search for Cubit is an internal operation, not a user/SConstruct operation

--- waves/tests/test_utilities.py
+++ waves/tests/test_utilities.py
@@ -42,3 +42,55 @@ quote_spaces_in_path_input = {
                          ids=quote_spaces_in_path_input.keys())
 def test_quote_spaces_in_path(path, expected):
     assert _utilities._quote_spaces_in_path(path) == expected
+
+
+def test_search_commands():
+    """Test :meth:`turbo_turtle._utilities.search_command`"""
+    with patch("shutil.which", return_value=None) as shutil_which:
+        command_abspath = _utilities.search_commands(["notfound"])
+        assert command_abspath is None
+
+    with patch("shutil.which", return_value="found") as shutil_which:
+        command_abspath = _utilities.search_commands(["found"])
+        assert command_abspath == "found"
+
+
+find_command = {
+    "first": (
+        ["first", "second"], "first", does_not_raise()
+    ),
+    "second": (
+        ["first", "second"], "second", does_not_raise()
+    ),
+    "none": (
+        ["first", "second"], None, pytest.raises(FileNotFoundError)
+    ),
+}
+
+
+@pytest.mark.parametrize("options, found, outcome",
+                         find_command.values(),
+                         ids=find_command.keys())
+def test_find_command(options, found, outcome):
+    """Test :meth:`turbo_turtle._utilities.find_command`"""
+    with patch("turbo_turtle._utilities.search_commands", return_value=found), outcome:
+        try:
+            command_abspath = _utilities.find_command(options)
+            assert command_abspath == found
+        finally:
+            pass
+
+
+def test_cubit_os_bin():
+    with patch("platform.system", return_value="Darwin"):
+        bin_directory = _utilities.cubit_os_bin()
+        assert bin_directory == "MacOS"
+
+    with patch("platform.system", return_value="Linux"):
+        bin_directory = _utilities.cubit_os_bin()
+        assert bin_directory == "bin"
+
+    # TODO: Find the Windows bin directory name, update the function and the test.
+    with patch("platform.system", return_value="Windows"):
+        bin_directory = _utilities.cubit_os_bin()
+        assert bin_directory == "bin"
