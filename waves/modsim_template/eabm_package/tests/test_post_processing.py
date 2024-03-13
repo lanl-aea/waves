632fc862ebdb3b8bf9ccc96a0bf9c90947ccae21
Kyle Brindley <kbrindley@lanl.gov>
2024-03-13 10:08:24 -0600

    WIP: draft post processing unit tests from the tutorial

---
+++ waves/modsim_template/eabm_package/tests/test_post_processing.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+import pandas
+
+from eabm_package import post_processing
+
+
+def test_csv_files_match():
+    data = {
+        'DummyLine': [1, 2, 3],
+        'SecondDummyLine': [4, 5, 6]
+    }
+    # Control DataFrame
+    control = pandas.DataFrame(data)
+
+    # Identical DataFrame
+    identical_copy = control.copy()
+
+    # Different DataFrame
+    different_copy = control.copy()
+    different_copy.loc[0, 'DummyLine'] = 999
+
+    # Assert that the function returns False when the DataFrames differ
+    assert post_processing.csv_files_match(control, different_copy) is False
+
+    # Assert that the function returns True when the DataFrames are identical
+    assert post_processing.csv_files_match(control, identical_copy) is True
