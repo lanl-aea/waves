b2cfaac7a2836cac716f95c4da4afc68c75ddb7c
Kyle Brindley <kbrindley@lanl.gov>
2024-03-13 12:21:20 -0600

    TST: sign-of-life test for merging a dataset and a waves parameter study object

--- waves/modsim_template/eabm_package/tests/test_utilities.py
+++ waves/modsim_template/eabm_package/tests/test_utilities.py
@@ -4,6 +4,7 @@ import unittest.mock
 import numpy
 import pandas
 import xarray
+import waves
 
 from eabm_package import utilities
 
@@ -26,6 +27,18 @@ def test_combine_data():
     with unittest.mock.patch("xarray.open_dataset", side_effect=xarray_side_effect):
         combined_data = utilities.combine_data(input_files, "/", "parameter_sets")
     assert combined_data.equals(expected)
+
+
+def test_merge_parameter_study():
+    parameter_study = waves.parameter_generators.CartesianProduct(
+        {"parameter_1": [1, 2], "parameter_2": [3, 4]}
+    ).parameter_study
+    combined_data = xarray.Dataset(
+        {"variable_name": (("parameter_sets", "space", "time"), numpy.array([[[1, 2, 3]], [[4, 5, 6]]]))},
+        coords={"space": [0], "time": [0, 1, 2], "parameter_sets": ["parameter_set0", "parameter_set1"]},
+    )
+    with unittest.mock.patch("xarray.open_dataset", return_value=parameter_study):
+        combined_data = utilities.merge_parameter_study("/mock/path/parameter_study.h5", combined_data)
 
 
 def test_csv_files_match():
