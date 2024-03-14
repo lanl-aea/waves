2c8aeec163fc72136e9a09dbcb965d09ab89641a
Kyle Brindley <kbrindley@lanl.gov>
2024-03-14 16:12:29 -0600

    MAINT: api documented salib sampler schema for correlation coefficients tutorial

--- waves/tutorials/eabm_package/python/correlation_coefficients_schema.py
+++ waves/tutorials/eabm_package/python/correlation_coefficients_schema.py
@@ -1,10 +1,27 @@
-"""Rectangle compression correlation coefficients WAVES SALibSampler Sobol parameter schema"""
-parameter_schema = {
-    'N': 5,
-    'problem': {
-        'num_vars': 2,
-        'names': ['width', 'height'],
-        'bounds': [[1., 0.1], [1., 0.1]],
-        'dists': ['norm', 'norm']
+"""Parameter sets and schemas for the rectangle compression simulation"""
+
+
+def schema(N=5,
+           width_distribution="norm", width_bounds=[1., 0.1],
+           height_distribution="norm", height_bounds[1., 0.1]):
+    """Return WAVES SALibSampler Sobol schema dictionary
+
+    :param int N: Number of parameter sets to sample
+    :param str width_distribution: SALib distribution name
+    :param list width_bounds: Distribution characteristics. Meaning varies depending on distribution type.
+    :param str height_distribution: SALib distribution name
+    :param list height_bounds: Distribution characteristics. Meaning varies depending on distribution type.
+
+    :returns: WAVES SALibSampler Sobol schema
+    :rtype: dict
+    """
+    schema = {
+        'N': 5,
+        'problem': {
+            'num_vars': 2,
+            'names': ['width', 'height'],
+            'bounds': [width_bounds, heigh_bounds],
+            'dists': [width_distribution, height_distribution]
+        }
     }
-}
+    return schema
