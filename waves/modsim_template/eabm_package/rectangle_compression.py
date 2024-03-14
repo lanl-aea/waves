29925b1bb9490e1203f459d889f4d401f11bd0d7
Kyle Brindley <kbrindley@lanl.gov>
2024-03-14 16:14:13 -0600

    MAINT: PEP-8 style

--- waves/modsim_template/eabm_package/rectangle_compression.py
+++ waves/modsim_template/eabm_package/rectangle_compression.py
@@ -20,6 +20,7 @@ def nominal(width=1.0, height=1.0, global_seed=1.0, displacement=-0.01):
     }
     return simulation_variables
 
+
 def mesh_convergence(global_seed=[1.0, 0.5, 0.25, 0.125]):
     """Return mesh convergence WAVES CartesianProduct schema dictionary
     :param float global_seed: The global mesh seed size
