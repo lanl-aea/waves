942ada7f3694c30ba2573540b32b83015c919001
Kyle Brindley <kbrindley@lanl.gov>
2024-03-14 15:37:55 -0600

    DOC: return parameter sets/schemas from functions to aid in default value documentation

--- waves/modsim_template/eabm_package/rectangle_compression.py
+++ waves/modsim_template/eabm_package/rectangle_compression.py
@@ -1,22 +1,33 @@
-"""Parameter sets and schemas for the rectangle compression simulation
+"""Parameter sets and schemas for the rectangle compression simulation"""
 
-* nominal: simulation variables dictionary
 
-  * width
-  * height
-  * global_seed
-  * displacement
+def nominal(width=1.0, height=1.0, global_seed=1.0, displacement=-0.01):
+    """Return nominal simulation variables dictionary
 
-* mesh_convergence: WAVES CartesianProduct schema
+    :param float width: The rectangle width
+    :param float height: The rectangle height
+    :param float global_seed: The global mesh seed size
+    :param float displacement: The rectangle top surface displacement
 
-  * global_seed
-"""
-nominal = {
-    'width': 1.0,
-    'height': 1.0,
-    'global_seed': 1.0,
-    'displacement': -0.01
-}
-mesh_convergence = {
-    'global_seed': [1.0, 0.5, 0.25, 0.125],
-}
+    :returns: nominal simulation variables
+    :rtype: dict
+    """
+    simulation_variables = {
+        'width': width,
+        'height': height,
+        'global_seed': global_seed,
+        'displacement': displacement
+    }
+    return simulation_variables
+
+def mesh_convergence(global_seed=[1.0, 0.5, 0.25, 0.125]):
+    """Return mesh convergence WAVES CartesianProduct schema dictionary
+    :param float global_seed: The global mesh seed size
+
+    :returns: WAVES CartesianProduct schema
+    :rtype: dict
+    """
+    schema = {
+        'global_seed': global_seed,
+    }
+    return schema
