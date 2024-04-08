d335f659318d61a7fa2ced654c29e8d2fc91dd92
Kyle Brindley <kbrindley@lanl.gov>
2024-04-08 09:10:55 -0600

    TST: move tests of docs module to dedicated test file

---
+++ waves/tests/test_docs.py
@@ -0,0 +1,30 @@
+from unittest.mock import patch
+from contextlib import nullcontext as does_not_raise
+
+import pytest
+
+from waves import _docs
+
+
+@pytest.mark.unittest
+def test_docs():
+    with patch('webbrowser.open') as mock_webbrowser_open:
+        _docs.main()
+        # Make sure the correct type is passed to webbrowser.open
+        mock_webbrowser_open.assert_called_with(str(_settings._installed_docs_index))
+
+    with patch('webbrowser.open') as mock_webbrowser_open, \
+         patch('pathlib.Path.exists', return_value=True), \
+         does_not_raise():
+        _docs.main(print_local_path=True)
+        mock_webbrowser_open.assert_not_called()
+
+    # Test the "unreachable" exit code used as a sign-of-life that the installed package structure assumptions in
+    # _settings.py are correct.
+    with patch('webbrowser.open') as mock_webbrowser_open, \
+         patch('pathlib.Path.exists', return_value=False), \
+         pytest.raises(RuntimeError):
+        try:
+            _docs.main(print_local_path=True)
+        finally:
+            mock_webbrowser_open.assert_not_called()
