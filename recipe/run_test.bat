bba722cfb0f9970667c1d7799851bf425588ea76
Kyle Brindley <kbrindley@lanl.gov>
2024-04-25 17:13:44 -0600

    MAINT: match pytest group separation found in internal testing

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -6,4 +6,5 @@ cd %modsim_template_directory%
 scons -h
 waves visualize nominal --output-file nominal.svg
 cd %SP_DIR%\%PKG_NAME%
-pytest
+pytest -vvv -n 4 -m "not programoperations and not systemtest"
+pytest -vvv -n 4 -m "programoperations"
