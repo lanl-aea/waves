e83660c6cabf91ec7dfb6ac2616f1ab498cbd984
Kyle Brindley <kbrindley@lanl.gov>
2024-05-07 16:05:17 -0600

    WIP: match API/CLI argument descriptions

--- waves/tutorials/modsim_package/abaqus/rectangle_geometry.py
+++ waves/tutorials/modsim_package/abaqus/rectangle_geometry.py
@@ -59,8 +59,8 @@ def get_parser():
     parser = argparse.ArgumentParser(description=cli_description,
                                      prog=prog)
     parser.add_argument('--output-file', type=str, default=default_output_file,
-                        help="The output file for the Abaqus model without extension. Will be appended with the " \
-                             "required extension, e.g. ``output_file``.cae")
+                        help="The output file for the Abaqus model. Will be stripped of the extension and ``.cae`` " \
+                             "will be used, e.g. ``output_file``.cae")
     parser.add_argument('--model-name', type=str, default=default_part_name,
                         help="The name of the Abaqus model")
     parser.add_argument('--part-name', type=str, default=default_part_name,
--- waves/tutorials/modsim_package/cubit/rectangle_geometry.py
+++ waves/tutorials/modsim_package/cubit/rectangle_geometry.py
@@ -44,8 +44,8 @@ def get_parser():
     parser = argparse.ArgumentParser(description=cli_description,
                                      prog=prog)
     parser.add_argument('--output-file', type=str, default=default_output_file,
-                        help="The output file for the Cubit model without extension. Will be appended with the " \
-                             "required extension, e.g. ``output_file``.cub")
+                        help="The output file for the Cubit model. Will be stripped of the extension and ``.cae`` " \
+                             "will be used, e.g. ``output_file``.cub")
     parser.add_argument('--width', type=float, default=default_width,
                         help="The rectangle width")
     parser.add_argument('--height', type=float, default=default_height,
