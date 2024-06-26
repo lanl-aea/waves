17ad569f575dbca631837b0b8826dd3cc47278e5
Kyle Brindley <kbrindley@lanl.gov>
2024-06-26 12:00:16 -0600

    TST: add new modsim template to system tests

--- waves/tests/test_systemtest.py
+++ waves/tests/test_systemtest.py
@@ -129,11 +129,12 @@ def test_run_tutorial(commands: typing.Iterable[str], fetch_options: typing.Opti
             subprocess.check_output(command, env=env, cwd=temp_directory).decode("utf-8")
 
 
+@pytest.mark.parametrize("template_name", ["modsim_template", "modsim_template_2"])
 @pytest.mark.systemtest
-def test_modsim_template() -> None:
+def test_modsim_templates(template_name) -> None:
     """Fetch and run the modsim template as a system test in a temporary directory"""
     with tempfile.TemporaryDirectory() as temp_directory:
-        command = f"{waves_command} fetch modsim_template --destination {temp_directory}"
+        command = f"{waves_command} fetch {template_name} --destination {temp_directory}"
         command = shlex.split(command)
         subprocess.check_output(command, env=env, cwd=temp_directory).decode("utf-8")
 
