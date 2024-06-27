50e7ea301cdab6901761182f6d50ff34dc14f8af
Kyle Brindley <kbrindley@lanl.gov>
2024-06-27 12:18:57 -0600

    TST: remove the deprecated fierro system test in favor of cubit+fierro tutorials

--- waves/tests/test_systemtest.py
+++ waves/tests/test_systemtest.py
@@ -69,7 +69,6 @@ system_tests = [
         [fetch_template, "scons quinoa-local --unconditional-build --print-build-failures"], "tutorials/tutorial_quinoa",
         marks=pytest.mark.skip("Re-enable quinoa tests when AEA server deployment stabilizes")
     ),
-    ([fetch_template, "scons . --unconditional-build --print-build-failures"], "tutorials/tutorial_fierro"),
     ([fetch_template, "scons tutorial_escape_sequences --sconstruct=tutorial_escape_sequences_SConstruct --solve-cpus=1 --unconditional-build --print-build-failures"], "tutorials"),
     ([fetch_template, "scons tutorial_builder_post_actions --sconstruct=tutorial_builder_post_actions_SConstruct --unconditional-build --print-build-failures"], "tutorials"),
     # TODO: Figure out how to authenticate the institutional account without expanding the user credential exposure to
