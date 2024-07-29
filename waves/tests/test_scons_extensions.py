8ba7250498a9d9543ca645b9d675f48e8fbe5a6e
Kyle Brindley <kbrindley@lanl.gov>
2024-07-29 13:43:50 -0600

    WIP: fix post action prefix test condition. required keyword may clash with the SCons builder attributes or environment?

--- waves/tests/test_scons_extensions.py
+++ waves/tests/test_scons_extensions.py
@@ -392,10 +392,9 @@ def test_abaqus_journal(program, post_action, node_count, action_count, target_l
     env.Append(BUILDERS={"AbaqusJournal": scons_extensions.abaqus_journal(program, post_action)})
     nodes = env.AbaqusJournal(target=target_list, source=["journal.py"], journal_options="")
     check_action_string(nodes, post_action, node_count, action_count, expected_string,
-                        post_action_prefix="${cd_action_prefix")
+                        post_action_prefix="${cd_action_prefix}")
     for node in nodes:
         assert node.env["program"] == program
-        assert node.env["required"] == "cae -noGui ${SOURCE.abspath}"
         assert node.env["cd_action_prefix"] == _cd_action_prefix
         assert node.env["redirect_environment_postfix"] == _redirect_environment_postfix
         assert node.env["redirect_action_postfix"] == _redirect_action_postfix
