4e6aa62126b38e5299ca0364c72ebf0a177fbb31
Kyle Brindley <kbrindley@lanl.gov>
2024-08-08 12:50:30 -0600

    TST: add system test for Cubit Python interpreter

--- waves/tests/test_systemtest.py
+++ waves/tests/test_systemtest.py
@@ -63,6 +63,7 @@ system_tests = [
     ([fetch_template, "scons tutorial_03_solverprep --sconstruct=tutorial_03_solverprep_SConstruct --unconditional-build --print-build-failures"], "--tutorial 3"),
     ([fetch_template, "scons tutorial_04_simulation --sconstruct=tutorial_04_simulation_SConstruct --unconditional-build --print-build-failures"], "--tutorial 4"),
     ([fetch_template, "scons . --unconditional-build --print-build-failures"], "tutorials/tutorial_cubit"),
+    ([fetch_template, "scons . --unconditional-build --print-build-failures"], "tutorials/tutorial_cubit_alternate"),
     # TODO: Re-enable quinoa tests when AEA server deployment stabilizes
     # https://re-git.lanl.gov/aea/python-projects/waves/-/issues/657
     pytest.param(
