3119d13ec5f1607e0edc6fb000171ab787345c05
Kyle Brindley <kbrindley@lanl.gov>
2024-09-17 07:53:10 -0600

    FEAT: run WAVES system tests that don't require third-party commands in external Conda package recipe

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,9 +1,1 @@
-waves docs --print-local-path
-waves fetch tutorials --destination waves_tutorials
-SET modsim_template_directory=modsim_template_directory
-waves fetch modsim_template --destination %modsim_template_directory%
-cd %modsim_template_directory%
-scons -h
-waves visualize nominal --output-file nominal.svg
-cd %SP_DIR%\%PKG_NAME%
-pytest -vvv -n 4 -m "not systemtest"
+pytest -vvv -n 4 -m "not require_third_party"
