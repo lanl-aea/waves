585f345d35c862969a12cf2c3652895da22b2e01
Kyle Brindley <kbrindley@lanl.gov>
2024-09-17 15:00:30 -0600

    MAINT: separate the test scripts for the external recipe and the internal matrixed recipe

--- recipe-matrix/run_test.sh
+++ recipe-matrix/run_test.sh
@@ -1,1 +1,3 @@
-../recipe/run_test.sh
+cd $SP_DIR/$PKG_NAME
+pytest -vvv -n 4 -m "not require_third_party"
+pytest -v -n 4 -m "systemtest and require_third_party" --tb=short
--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,4 +1,2 @@
 cd %SP_DIR%\%PKG_NAME%
 pytest -vvv -n 4 -m "not require_third_party"
-pytest -v -n 4 -m "systemtest" --tb=short
-pytest -v -n 4 -m "systemtest and require_third_party" --tb=short
