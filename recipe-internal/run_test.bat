3b8bb83c30785f0d29a87662832920f46bc646f9
Kyle Brindley <kbrindley@lanl.gov>
2024-09-26 14:39:18 -0600

    MAINT: continue testing conda-packages in /tmp. The PWD of the conda-package environment is too long for some system test files

--- recipe-internal/run_test.bat
+++ recipe-internal/run_test.bat
@@ -1,4 +1,4 @@
 pip check
 cd %SP_DIR%\%PKG_NAME%
-pytest -vvv -n 4 -m "not require_third_party" --system-test-dir=%SP_DIR%\%PKG_NAME%\build\pytest
-pytest -v -n 4 -m "systemtest and require_third_party" --tb=short --system-test-dir=%SP_DIR%\%PKG_NAME%\build\pytest
+pytest -vvv -n 4 -m "not require_third_party"
+pytest -v -n 4 -m "systemtest and require_third_party" --tb=short
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,3 +1,3 @@
 pip check
 cd $SP_DIR/$PKG_NAME
-pytest -vvv -n 4 -m "not require_third_party" --system-test-dir=$SP_DIR/$PKG_NAME/build/pytest
+pytest -vvv -n 4 -m "not require_third_party"
