e9d2e4165490a7b0a5f355d4f9ee1283fa8e9a60
Kyle Brindley <kbrindley@lanl.gov>
2024-10-01 07:58:28 -0600

    MAINT: inline the defaults to clarify that they aren't expected to change

--- waves/modsim_template/modsim_package/tests/test_rectangle_compression.py
+++ waves/modsim_template/modsim_package/tests/test_rectangle_compression.py
@@ -5,24 +5,22 @@ from modsim_package import rectangle_compression
 
 nominal_tests = {
     "default": (
-        {"width": 1.0, "height": 1.0, "global_seed": 1.0, "displacement": -0.01},
         {},
         {"width": 1.0, "height": 1.0, "global_seed": 1.0, "displacement": -0.01},
     ),
     "override all": (
-        {"width": 1.0, "height": 1.0, "global_seed": 1.0, "displacement": -0.01},
         {"width": 2.0, "height": 3.0, "global_seed": 4.0, "displacement": -0.05},
         {"width": 2.0, "height": 3.0, "global_seed": 4.0, "displacement": -0.05},
     )
 }
 
 
-@pytest.mark.parametrize("default_kwargs, kwargs, expected",
+@pytest.mark.parametrize("kwargs, expected",
                          nominal_tests.values(),
                          ids=nominal_tests.keys())
-def test_nominal(default_kwargs, kwargs, expected):
+def test_nominal(kwargs, expected):
     # Set default expectations to match default argument values
-    expected_kwargs = default_kwargs
+    expected_kwargs = {"width": 1.0, "height": 1.0, "global_seed": 1.0, "displacement": -0.01}
     # Update expected arguments to match test case
     expected_kwargs.update(kwargs)
 
@@ -32,24 +30,24 @@ def test_nominal(default_kwargs, kwargs, expected):
 
 mesh_convergence_tests = {
     "default": (
-        {"width": [1.0], "height": [1.0], "global_seed": [1.0, 0.5, 0.25, 0.125], "displacement": [-0.01]},
         {},
         {"width": [1.0], "height": [1.0], "global_seed": [1.0, 0.5, 0.25, 0.125], "displacement": [-0.01]},
     ),
     "override all": (
-        {"width": [1.0], "height": [1.0], "global_seed": [1.0, 0.5, 0.25, 0.125], "displacement": [-0.01]},
         {"width": [2.0], "height": [3.0], "global_seed": [4.0, 2.0], "displacement": [-0.05]},
         {"width": [2.0], "height": [3.0], "global_seed": [4.0, 2.0], "displacement": [-0.05]},
     )
 }
 
 
-@pytest.mark.parametrize("default_kwargs, kwargs, expected",
+@pytest.mark.parametrize("kwargs, expected",
                          mesh_convergence_tests.values(),
                          ids=mesh_convergence_tests.keys())
-def test_mesh_convergence(default_kwargs, kwargs, expected):
+def test_mesh_convergence(kwargs, expected):
     # Set default expectations to match default argument values
-    expected_kwargs = default_kwargs
+    expected_kwargs = \
+        {"width": [1.0], "height": [1.0], "global_seed": [1.0, 0.5, 0.25, 0.125], "displacement": [-0.01]}
+
     # Update expected arguments to match test case
     expected_kwargs.update(kwargs)
 
