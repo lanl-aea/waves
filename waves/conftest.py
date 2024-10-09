de645d009583314ef60a87c4f9f8cf3138d56b47
Kyle Brindley <kbrindley@lanl.gov>
2024-10-09 12:46:27 -0600

    WIP: add a pytest unconditional build option

--- waves/conftest.py
+++ waves/conftest.py
@@ -11,8 +11,19 @@ def pytest_addoption(parser):
         default=None,
         help="system test build directory root"
     )
+    parser.addoption(
+        "--unconditional-build",
+        action="store_true",
+        default=False,
+        help="pass unconditional build option to sytem tests"
+    )
 
 
 @pytest.fixture
 def system_test_directory(request):
     return request.config.getoption("--system-test-dir")
+
+
+@pytest.fixture
+def unconditional_build(request):
+    return request.config.getoption("--unconditional-build")
