93806e588ef933e7e0eca76b4eced59d0734575b
Kyle Brindley <kbrindley@lanl.gov>
2024-10-11 15:35:15 -0600

    FEAT: improved error handling in extract script

--- waves/tutorials/tutorial_gmsh/extract.py
+++ waves/tutorials/tutorial_gmsh/extract.py
@@ -1,17 +1,26 @@
 import re
+import sys
 import pathlib
 import argparse
 
 import ccx2paraview.ccx2paraview
 
 
-def main(input_file: pathlib.Path, output_formats: list) -> None:
+required_input_file_suffix = ".frd"
+default_output_format = "vtu"
+
+
+def main(input_file: pathlib.Path, output_format: str = default_output_format) -> None:
     """Convert CalculiX FRD results file to Paraview VTK file with ccx2paraview
 
     :param input_file: The CalculiX FRD file to read
-    :param output_formats: The ParaView output format(s): 'vtu', 'vtk', or both
+    :param output_format: The ParaView output format: 'vtu' or 'vtk'
     """
-    converter = ccx2paraview.ccx2paraview.Converter(str(input_file), output_formats)
+    if not input_file.suffix == required_input_file_suffix:
+        sys.exit(f"Input file '{input_file}' must have suffix '{required_input_file_suffix}'")
+    if not input_file.is_file():
+        sys.exit(f"No such file '{input_file}' found")
+    converter = ccx2paraview.ccx2paraview.Converter(str(input_file), [output_format])
     converter.run()
 
 
@@ -27,11 +36,10 @@ def get_parser():
         required=True
     )
     parser.add_argument(
-        "--output-formats",
+        "--output-format",
         type=str,
-        nargs="+",
-        default=["vtu"],
-        required=True
+        choices=["vtu", "vtk"],
+        default=default_output_format
     )
     return parser
 
@@ -41,5 +49,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     main(
         input_file=args.input_file,
-        output_formats=args.output_formats
+        output_format=args.output_format
     )
