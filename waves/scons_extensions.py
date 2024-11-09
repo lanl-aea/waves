ff7bdcb6793d6016265292c042b485a05d3fae00
Matthew Fister <mwfister@lanl.gov>
2024-11-09 07:45:50 -0700

    Pass input file as str always. Account for periods in extensions list.

--- waves/scons_extensions.py
+++ waves/scons_extensions.py
@@ -1641,23 +1641,23 @@ class AbaqusPseudoBuilder:
         if inp:
             options += f" input={inp}"
         else:
-            inp = pathlib.Path(job).with_suffix('.inp')
+            inp = str(pathlib.Path(job).with_suffix('.inp'))
         # Include input file as first source
         sources.append(inp)
 
-        targets.extend([f"{job}.{ext}" for ext in _settings._abaqus_standard_extensions])
+        targets.extend([f"{job}{ext}" for ext in _settings._abaqus_standard_extensions])
 
         # Always allow user to override CPUs with CLI option and exclude CPUs from build signature
         options += f" $(cpus={self.override_cpus or cpus}$)"
 
         # If restarting a job, add old job restart files to sources
         if oldjob:
-            sources.extend([f"{oldjob}.{ext}" for ext in _settings._abaqus_restart_extensions])
+            sources.extend([f"{oldjob}{ext}" for ext in _settings._abaqus_restart_extensions])
             options += f' oldjob={oldjob}'
 
         # If writing restart files, add restart files to targets
         if write_restart:
-            targets.extend([f"{job}.{ext}" for ext in _settings._abaqus_restart_extensions])
+            targets.extend([f"{job}{ext}" for ext in _settings._abaqus_restart_extensions])
 
         # If user subroutine is specified, add user subroutine to sources
         if user:
