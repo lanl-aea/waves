3e6c9939ef895f76732c92bd84c5772cb27577a7
Sergio Cordova <sergioc@lanl.gov>
2025-01-29 12:07:53 -0700

    Updated non-default target description tests.

--- waves/_tests/test_help_messages.py
+++ waves/_tests/test_help_messages.py
@@ -113,15 +113,6 @@ def test_project_help_message():
         patch("waves.scons_extensions.default_targets_message") as mock_targets,
         patch("waves.scons_extensions.alias_list_message") as mock_alias,
     ):
-        scons_extensions.project_help_message(env=env, append=False, keep_local=False)
-        mock_targets.assert_called_once_with(env=env, append=False, keep_local=False, target_descriptions=None)
-        mock_alias.assert_called_once_with(env=env, append=False, keep_local=False, target_descriptions=None)
-
-    # Test non-default target_descriptions
-    with (
-        patch("waves.scons_extensions.default_targets_message") as mock_targets,
-        patch("waves.scons_extensions.alias_list_message") as mock_alias,
-    ):
         descriptions = {"somekey": "somevalue"}
         scons_extensions.project_help_message(env=env, append=False, keep_local=False, target_descriptions=descriptions)
         mock_targets.assert_called_once_with(env=env, append=False, keep_local=False, target_descriptions=descriptions)
