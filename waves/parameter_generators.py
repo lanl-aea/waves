65e26dc921f74120e1b7036200a01324b0dd5c8d
Christopher Lin Johnson <clj@lanl.gov>
2025-03-04 17:21:15 -0700

    WIP: use black formatting for warning message

--- waves/parameter_generators.py
+++ waves/parameter_generators.py
@@ -1557,8 +1557,8 @@ def _coerce_values(values: typing.Iterable, name: typing.Optional[str] = None) -
     datatypes = set([type(value) for value in values])
     values_coerced = numpy.array(values)
     if len(datatypes) > 1:
-        warnings.warn(f'Found mixed datatypes in parameter {name}: {datatypes}. Values will be converted to '
-                      f'{type(values_coerced[0])}.')
+        warnings.warn(f"Found mixed datatypes in parameter '{name}': '{datatypes}'. Values will be converted to "
+                      f"'{type(values_coerced[0])}'.")
     return values_coerced
 
 
