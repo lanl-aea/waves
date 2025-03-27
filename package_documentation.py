c09a18324ff307043de8b866d2dbac357d1330a7
Kyle Brindley <kbrindley@lanl.gov>
2025-03-27 15:05:29 -0600

    FEAT: add a command line interface to the package documentation script

--- package_documentation.py
+++ package_documentation.py
@@ -1,28 +1,58 @@
 import os
+import sys
 import shutil
 import pathlib
+import argparse
 
-prefix = pathlib.Path(os.getenv("PREFIX")).resolve()
-sp_dir = pathlib.Path(os.getenv("SP_DIR")).resolve()
-pkg_name = os.getenv("PKG_NAME")
 
-man_path = pathlib.Path("build/docs/man/waves.1").resolve()
-html_path_external = pathlib.Path("build/docs/html").resolve()
-html_path_internal = pathlib.Path("build/docs/html-internal").resolve()
-html_path = html_path_external if html_path_external.exists() else html_path_internal
-# TODO: figure out the setuptools and setuptools_scm solution for including data files
-# FIXME: setuptools claims that these should be included by default, but they aren't
-readme_path = pathlib.Path("README.rst").resolve()
-pyproject = pathlib.Path("pyproject.toml").resolve()
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--prefix",
+        type=pathlib.Path,
+        default=None,
+        help="Installation environment prefix. If 'None', use environment variable 'PREFIX' (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--sp-dir",
+        type=pathlib.Path,
+        default=None,
+        help=(
+            "Installation environment site-packages full path, including prefix. "
+            "If 'None', use environment variable 'SP_DIR' (default: %(default)s)"
+        ),
+    )
+    parser.add_argument(
+        "--pkg-name",
+        default=None,
+        help="Package name. If 'None', use environment variable 'PKG_NAME' (default: %(default)s)",
+    )
+    return parser
 
-new_paths = [
-    (prefix / "share/man/man1", man_path),
-    (prefix / "man/man1", man_path),
-    (sp_dir / pkg_name / "docs", html_path),
-    (sp_dir / pkg_name / "README.rst", readme_path),
-    (sp_dir / pkg_name / "pyproject.toml", pyproject),
-]
-for destination, source in new_paths:
+
+def validate_input(prefix, sp_dir, pkg_name):
+    """Validate arguments and raise exceptions when
+
+    :raises RuntimeError: prefix, sp_dir, or pkg_name are None or empty
+    :raises FileNotFoundError: prefix or sp_dir directories do not exist
+    """
+    if not prefix:
+        raise RuntimeError("PREFIX not specified or not found in the environment variable 'PREFIX'")
+    if not prefix.is_dir():
+        raise FileNotFoundError("PREFIX '{prefix}' is not a directory")
+    if not sp_dir:
+        raise RuntimeError("SP_DIR not specified or not found in the environment variable 'SP_DIR'")
+    if not sp_dir.is_dir():
+        raise FileNotFoundError("SP_DIR '{sp_dir}' is not a directory")
+    if not pkg_name:
+        raise RuntimeError("PKG_NAME not specified or not found in the environment variable 'PKG_NAME'")
+
+
+def copy_paths(destination, source):
+    """Copy files and recursively copy directories
+
+    :raises FileNotFoundError: if source does not exist
+    """
     if not source.exists():
         raise FileNotFoundError(f"'{source}' does not exist")
     print(f"Copying '{source}' to '{destination}'...")
@@ -38,3 +68,40 @@ for destination, source in new_paths:
             dirs_exist_ok=True,
             ignore=shutil.ignore_patterns(".doctrees", "*.doctree", ".buildinfo"),
         )
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+    prefix = args.prefix if args.prefix is not None else pathlib.Path(os.getenv("PREFIX")).resolve()
+    sp_dir = args.sp_dir if args.sp_dir is not None else pathlib.Path(os.getenv("SP_DIR")).resolve()
+    pkg_name = args.pkg_name if args.pkg_name is not None else os.getenv("PKG_NAME")
+
+    validate_input(prefix, sp_dir, pkg_name)
+
+    man_path = pathlib.Path("build/docs/man/waves.1").resolve()
+    html_path_external = pathlib.Path("build/docs/html").resolve()
+    html_path_internal = pathlib.Path("build/docs/html-internal").resolve()
+    html_path = html_path_external if html_path_external.exists() else html_path_internal
+    # TODO: figure out the setuptools and setuptools_scm solution for including data files
+    # FIXME: setuptools claims that these should be included by default, but they aren't
+    readme_path = pathlib.Path("README.rst").resolve()
+    pyproject = pathlib.Path("pyproject.toml").resolve()
+
+    new_paths = [
+        (prefix / "share/man/man1", man_path),
+        (prefix / "man/man1", man_path),
+        (sp_dir / pkg_name / "docs", html_path),
+        (sp_dir / pkg_name / "README.rst", readme_path),
+        (sp_dir / pkg_name / "pyproject.toml", pyproject),
+    ]
+    for destination, source in new_paths:
+        try:
+            copy_paths(destination, source)
+        except RuntimeError, FileNotFoundError as err:
+            sys.exit(err)
+
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
