6f8599464b0e29c4a251b61be9f58a546fb52f0f
Kyle Brindley <kbrindley@lanl.gov>
2025-04-09 12:51:11 -0600

    WIP: remove a couple of directories in the internal conda-build system test directory. Hoping that the persistent test failures are related to the longer path introduced by the gitlab-ci working directory environment

--- recipe-internal/run_test.sh
+++ recipe-internal/run_test.sh
@@ -1,5 +1,6 @@
 pip check
 working_directory=$PWD
+if [[ -z ${CI_PROJECT_DIR} ]]; then working_directory=${CI_PROJECT_DIR}; fi
 cd $SP_DIR/$PKG_NAME
 pytest -vvv -n 4 -m "not require_third_party" --system-test-dir=${working_directory} --unconditional-build
 pytest -v -n 4 -m "systemtest and require_third_party" --tb=short --system-test-dir=${working_directory} --unconditional-build
