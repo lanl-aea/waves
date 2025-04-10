66d48eeb559ff2361f9aecb9e7e8d7d7391bdf77
Kyle Brindley <kbrindley@lanl.gov>
2025-04-10 08:18:47 -0600

    WIP: try using the test source directory as the working directory. Only a couple of directories higher, but it might make the difference and it is a variable native to the conda-build process, so system agnostic

--- recipe-internal/run_test.sh
+++ recipe-internal/run_test.sh
@@ -1,8 +1,5 @@
 pip check
-# FIXME: Set a system agnostic path if this shorter working directory fixes the system test failures.
-# May need to pass in working directory as an environment variable through the recipe test environment.
-# https://re-git.lanl.gov/aea/python-projects/waves/-/merge_requests/1163
-working_directory="/scratch/$USER/waves/recipe-internal"
-cd $SP_DIR/$PKG_NAME
+working_directory=${SRC_DIR}
+cd ${SP_DIR}/${PKG_NAME}
 pytest -vvv -n 4 -m "not require_third_party" --system-test-dir=${working_directory} --unconditional-build
 pytest -v -n 4 -m "systemtest and require_third_party" --tb=short --system-test-dir=${working_directory} --unconditional-build
