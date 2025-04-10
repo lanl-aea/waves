06e06b61cc32f1191fd86238ae392644353db176
Kyle Brindley <kbrindley@lanl.gov>
2025-04-10 08:36:53 -0600

    WIP: SRC_DIR not available to recipe test stage. Construct a build path on AEA CI server, falling back to PWD for all other recipe builds

--- recipe-internal/run_test.sh
+++ recipe-internal/run_test.sh
@@ -1,5 +1,8 @@
 pip check
-working_directory=${SRC_DIR}
+working_directory=${PWD}
+# Avoid file system maximum path length issues during CI testing. Can't use /tmp on CI server because some system tests require execute permissions.
+scratch_directory=/scratch/${USER}
+if [[ -z ${scratch_directory} ]] && [[ -d ${scratch_directory} ]]; then working_directory=${scratch_directory}/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILD_STRING}_${PKG_BUILDNUM}; fi
 cd ${SP_DIR}/${PKG_NAME}
 pytest -vvv -n 4 -m "not require_third_party" --system-test-dir=${working_directory} --unconditional-build
 pytest -v -n 4 -m "systemtest and require_third_party" --tb=short --system-test-dir=${working_directory} --unconditional-build
