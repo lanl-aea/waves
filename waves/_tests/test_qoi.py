5a11eedea6de47c997a8a8e1584e13f71d2edc5a
Matthew Fister <mwfister@lanl.gov>
2025-04-14 15:58:14 -0600

    Add unit tests. They all pass.

---
+++ waves/_tests/test_qoi.py
@@ -0,0 +1,110 @@
+"""Test QOI module"""
+
+import pathlib
+from unittest.mock import patch, mock_open, Mock
+from contextlib import nullcontext as does_not_raise
+
+import pytest
+import numpy
+import xarray
+
+import waves.qoi
+
+def test_create_qoi():
+    pass
+
+
+def test_create_qoi_set():
+    pass
+
+
+def test__create_qoi_study():
+    pass
+
+
+def test__qoi_group():
+    pass
+
+
+def test__create_qoi_archive():
+    pass
+
+
+def test__merge_qoi_archives():
+    pass
+
+
+def test__read_qoi_set():
+    pass
+
+
+def test__add_tolerance_attribute():
+    pass
+
+
+def test_write_qoi_set_to_csv():
+    pass
+
+
+def test__plot_qoi_tolerance_check():
+    pass
+
+
+def test__plot_scalar_tolerance_check():
+    pass
+
+
+def test__write_qoi_report():
+    pass
+
+
+def test__get_plotting_name():
+    pass
+
+
+def test__plot_scalar_qoi_history():
+    pass
+
+
+def test__qoi_history_report():
+    pass
+
+
+def test__get_commit_date():
+    pass
+
+
+def test__add_commit_date():
+    pass
+
+
+def test__sort_by_date():
+    pass
+
+
+def test__accept():
+    pass
+
+
+def test__check():
+    pass
+
+
+def test__diff():
+    pass
+
+
+def test__aggregate():
+    pass
+
+
+def test__report():
+    pass
+
+
+def test__plot_archive():
+    pass
+
+
+def test__archive():
+    pass
