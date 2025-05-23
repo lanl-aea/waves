5609a01d40184e8827a8ef68d51c2fb304d2c48c
Kyle Brindley <kbrindley@lanl.gov>
2025-05-23 14:27:58 -0600

    WIP: default tutorial qois fall within limits, but can be overridden

--- waves/tutorials/tutorial_qoi/sim_1_post.py
+++ waves/tutorials/tutorial_qoi/sim_1_post.py
@@ -1,34 +1,47 @@
+import argparse
+
 from waves import qoi
 
-# Create multiple QOIs
-load = qoi.create_qoi(
-    name="load",
-    calculated=5.0,
-    units="N",
-    long_name="Axial Load",
-    description="Axial load through component XYZ",
-    group="Assembly ABC Preload",
-    version="abcdef",
-    date="2025-01-01",
-)
-gap = qoi.create_qoi(
-    name="gap",
-    calculated=1.0,
-    units="mm",
-    long_name="Radial gap",
-    description="Radial gap between components A and B",
-    group="Assembly ABC Preload",
-    version="abcdef",
-    date="2025-01-01",
-)
 
-# Combine QOIs into calculated QOIs set
-sim_1_qois = qoi.create_qoi_set((load, gap))
-print(sim_1_qois)
-print(sim_1_qois["load"])
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--load", type=float, default=5.0)
+    parser.add_argument("--gap", type=float, default=0.85)
+    args = parser.parse_args()
 
-# Save calculated QOIs to CSV
-qoi.write_qoi_set_to_csv(sim_1_qois, "sim_1_qois.csv")
+    # Create multiple QOIs
+    load = qoi.create_qoi(
+        name="load",
+        calculated=args.load,
+        units="N",
+        long_name="Axial Load",
+        description="Axial load through component XYZ",
+        group="Assembly ABC Preload",
+        version="abcdef",
+        date="2025-01-01",
+    )
+    gap = qoi.create_qoi(
+        name="gap",
+        calculated=args.gap,
+        units="mm",
+        long_name="Radial gap",
+        description="Radial gap between components A and B",
+        group="Assembly ABC Preload",
+        version="abcdef",
+        date="2025-01-01",
+    )
 
-# Save calculated QOIs to h5
-sim_1_qois.to_netcdf("sim_1_qois.h5")
+    # Combine QOIs into calculated QOIs set
+    sim_1_qois = qoi.create_qoi_set((load, gap))
+    print(sim_1_qois)
+    print(sim_1_qois["load"])
+
+    # Save calculated QOIs to CSV
+    qoi.write_qoi_set_to_csv(sim_1_qois, "sim_1_qois.csv")
+
+    # Save calculated QOIs to h5
+    sim_1_qois.to_netcdf("sim_1_qois.h5")
+
+
+if __name__ == "__main__":
+    main()
