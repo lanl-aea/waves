386fe22ea7009182133ddf072c1343bd3565ae45
Kyle Brindley <kbrindley@lanl.gov>
2025-09-11 08:54:23 -0600

    WIP: ruff naming convention 'N' checks

--- recipe-matrix/matrix.py
+++ recipe-matrix/matrix.py
@@ -15,7 +15,7 @@ command_template = string.Template(
     "VERSION=$(python -m setuptools_scm) conda mambabuild recipe-matrix --channel fierromechanics "
     "--channel conda-forge --no-anaconda-upload "
     "${CROOT} ${OUTPUT_FOLDER} "
-    "--python ${PYTHON_VERSION} --variants \"{'scons':['${SCONS_VERSION}']}\""
+    "--python ${python_version} --variants \"{'scons':['${scons_version}']}\""
 )
 
 python_versions = ["3.10", "3.11", "3.12", "3.13"]
@@ -24,15 +24,15 @@ conda_build_test_matrix = list(itertools.product(python_versions, scons_versions
 conda_build_test_matrix.remove(("3.13", "4.6"))  # SCons 4.6 not available for Python 3.13
 
 
-@pytest.mark.parametrize("PYTHON_VERSION, SCONS_VERSION", conda_build_test_matrix)
-def test_matrix(PYTHON_VERSION: str, SCONS_VERSION: str) -> None:
+@pytest.mark.parametrize("python_version, scons_version", conda_build_test_matrix)
+def test_matrix(python_version: str, scons_version: str) -> None:
     template = command_template
     command = template.safe_substitute(
         {
             "OUTPUT_FOLDER": OUTPUT_FOLDER,
             "CROOT": CROOT,
-            "PYTHON_VERSION": PYTHON_VERSION,
-            "SCONS_VERSION": SCONS_VERSION,
+            "python_version": python_version,
+            "scons_version": scons_version,
         }
     )
     subprocess.check_output(
