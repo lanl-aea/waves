7a9b50d937bc229bcac82a2f6ee269cf80f0875e
Matthew Fister <mwfister@lanl.gov>
2025-09-23 08:54:43 -0600

    One more line length fix

--- waves/scons_extensions.py
+++ waves/scons_extensions.py
@@ -2136,7 +2136,7 @@ class AbaqusPseudoBuilder:
             # Check that total number of CPUs is evenly divisible by number of threads per process
             if cpus % processes != 0:
                 raise RuntimeError(
-                    f"The total number of CPUs ({cpus}) is not evenly divisible by the number of MPI processes ({processes})"
+                    f"Number of CPUs ({cpus}) is not evenly divisible by the number of MPI processes ({processes})"
                 )
             # Calculate number of threads per MPI process
             threads_per_mpi_process = cpus // processes
